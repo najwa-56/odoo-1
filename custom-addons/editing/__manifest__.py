d6900e106a4dd69e54845f32c8d55a1bf5b02b04
najwa-56 <najwa-56@hotmail.com>
2024-05-02 13:23:19 +0300

    editing

---
+++ custom-addons/editing/__manifest__.py
@@ -0,0 +1,25 @@
+
+# -*- coding: utf-8 -*-
+
+{
+    'name': ' editing',
+    'version': '1.0',
+    'category': 'Product',
+    'sequence': 6,
+    'author': 'najwa and abeer',
+    'summary': "",
+    'description': """
+
+=======================
+
+
+""",
+    'depends': ['sale_management','stock'],
+    'data': [
+        'views/editing.xml',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'website': '',
+    'auto_install': False,
+}
