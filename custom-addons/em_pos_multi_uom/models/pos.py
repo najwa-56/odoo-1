919fbd66e4adb75b6e61e3d2a63cf020950a1bd4
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-06-29 15:21:33 +0300

    pos

--- custom-addons/em_pos_multi_uom/models/pos.py
+++ custom-addons/em_pos_multi_uom/models/pos.py
@@ -125,6 +125,23 @@ class PosOrderLine(models.Model):
 
     product_uom = fields.Many2one('uom.uom','Unit of measure')
 
+    Ratio = fields.Float("Ratio", compute="_compute_ratio",
+                         store=False)  # Ratio field  # Related field to the ratio in uom.uom
+
+    @api.depends('product_uom')
+    def _compute_ratio(self):
+        for record in self:
+            record.Ratio = record.product_uom.ratio if record.product_uom else 1.0
+
+    def _compute_total_cost(self, stock_moves=None):
+        """
+        Compute the total cost of the order lines and multiply by the ratio.
+        :param stock_moves: recordset of `stock.move`, used for fifo/avco lines
+        """
+        super(PosOrderLine, self)._compute_total_cost(stock_moves)
+        for line in self:
+            line.total_cost = line.total_cost * line.Ratio if line.Ratio else line.total_cost
+
     def _export_for_ui(self, orderline):
         res = super(PosOrderLine, self)._export_for_ui(orderline)
         if orderline.product_uom:
