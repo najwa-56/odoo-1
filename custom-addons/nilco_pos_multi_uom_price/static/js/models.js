9f994f7ca7c1f96ede62a477924b1b0a10c86233
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-06-30 21:20:35 +0300

    Update models.js

--- custom-addons/nilco_pos_multi_uom_price/static/js/models.js
+++ custom-addons/nilco_pos_multi_uom_price/static/js/models.js
@@ -2,54 +2,70 @@
 import { Order, Orderline, Payment } from "@point_of_sale/app/store/models";
 import { patch } from "@web/core/utils/patch";
 import { PosStore } from "@point_of_sale/app/store/pos_store";
-patch(Order.prototype, { 
-  set_orderline_options(orderline, options) {
+
+patch(Order.prototype, {
+    set_orderline_options(orderline, options) {
         super.set_orderline_options(...arguments);
-        if(options.product_uom_id !== undefined){
+        if (options.product_uom_id !== undefined) {
             orderline.product_uom_id = options.product_uom_id;
         }
     }
 });
-patch(Orderline.prototype, {  
+
+patch(Orderline.prototype, {
     setup(_defaultObj, options) {
-        super.setup(...arguments);    
-        this.product_uom_id =  this.product_uom_id || this.product.uom_id;
+        super.setup(...arguments);
+        // Initialize product_uom_id if not already set
+        this.product_uom_id = this.product_uom_id || this.product.uom_id;
     },
-    
+
     export_as_JSON() {
         const json = super.export_as_JSON(...arguments);
-        json.product_uom_id = this.product_uom_id[0];
+        // Ensure product_uom_id exists before accessing its elements
+        if (this.product_uom_id) {
+            json.product_uom_id = this.product_uom_id[0]; // Assuming product_uom_id is an array
+        }
         return json;
     },
+
     init_from_JSON(json) {
         super.init_from_JSON(...arguments);
-        this.product_uom_id = {
-            0 : this.pos.units_by_id[json.product_uom_id].id,
-            1 : this.pos.units_by_id[json.product_uom_id].name
+        // Ensure json.product_uom_id is valid and this.pos.units_by_id is properly initialized
+        if (json.product_uom_id && this.pos && this.pos.units_by_id) {
+            this.product_uom_id = {
+                0: this.pos.units_by_id[json.product_uom_id].id,
+                1: this.pos.units_by_id[json.product_uom_id].name
+            };
         }
     },
+
     set_uom(uom_id) {
+        // Handle setting product_uom_id
         this.product_uom_id = uom_id;
     },
-    get_unit(){
-        if (this.product_uom_id){
+
+    get_unit() {
+        if (this.product_uom_id) {
             var unit_id = this.product_uom_id;
-            if(!unit_id){
+            if (!unit_id) {
                 return undefined;
             }
             unit_id = unit_id[0];
-            if(!this.pos){
+            if (!this.pos) {
                 return undefined;
             }
+            // Ensure pos.units_by_id is properly initialized
             return this.pos.units_by_id[unit_id];
         }
+        // Fallback to product.get_unit() if product_uom_id is not set
         return this.product.get_unit();
     }
 });
+
 patch(PosStore.prototype, {
     async _processData(loadedData) {
         await super._processData(...arguments);
-            this.product_uom_price = loadedData['product.multi.uom.price'];
+        // Ensure loadedData contains 'product.multi.uom.price' and initialize this.product_uom_price accordingly
+        this.product_uom_price = loadedData['product.multi.uom.price'];
     }
 });
-
