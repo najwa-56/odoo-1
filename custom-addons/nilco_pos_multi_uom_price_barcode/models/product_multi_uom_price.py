e63806d80593eb56022eabcea676ffe333200b6c
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-01 10:03:48 +0300

    Update product_multi_uom_price.py

--- custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
+++ custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
@@ -6,5 +6,7 @@ class Inheritmulti_uom(models.Model):
     _inherit = 'product.multi.uom.price'
     barcode = fields.Char('Barcode')
     product_variant_id =fields.Many2one('product.product',related="product_id.product_variant_id",store=True)
+    product_variant_count = fields.Integer('Product Variant Count')
+
  
     
