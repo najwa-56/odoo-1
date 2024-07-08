0a6e018e55e26767dae220f6974088970a0a8e3e
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-08 16:07:48 +0300

    7

--- custom-addons/nilco_pos_multi_uom_price/static/js/models.js
+++ custom-addons/nilco_pos_multi_uom_price/static/js/models.js
@@ -2,6 +2,8 @@
 import { Order, Orderline, Payment } from "@point_of_sale/app/store/models";
 import { patch } from "@web/core/utils/patch";
 import { PosStore } from "@point_of_sale/app/store/pos_store";
+import { _t } from '@web/core/l10n/translation';
+
 patch(Order.prototype, {
   set_orderline_options(orderline, options) {
         super.set_orderline_options(...arguments);
