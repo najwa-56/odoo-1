d4de176b6442e496b64a0a4958aed669fc688ec8
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-10 14:43:47 +0300

    1

--- custom-addons/ksa_zatca_integration_pos/static/src/js/pos.js
+++ custom-addons/ksa_zatca_integration_pos/static/src/js/pos.js
@@ -4,7 +4,7 @@ odoo.define('custom_pos.PosModel', function (require) {
     const models = require('point_of_sale.models');
     const session = require('web.session');
 
-    models.load_fields('pos.config', ['printer_ids']);
+    models.load_fields('pos.config', ['default_customer_id']);
 
     const _super_PosModel = models.PosModel.prototype;
     models.PosModel = models.PosModel.extend({
@@ -14,7 +14,7 @@ odoo.define('custom_pos.PosModel', function (require) {
 
             this.ready.then(function () {
                 if (self.config.default_customer_id) {
-                    const client = self.db.get_partner_by_id(self.config.printer_ids[0]);
+                    const client = self.db.get_partner_by_id(self.config.default_customer_id[0]);
                     if (client) {
                         self.get_order().set_client(client);
                     }
