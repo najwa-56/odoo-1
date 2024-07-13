9f5029398dc59d1322a2c2b7e2e191377d0098a9
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-13 17:46:58 +0300

    142

--- custom-addons/nilco_pos_multi_uom_price/models/product_template.py
+++ custom-addons/nilco_pos_multi_uom_price/models/product_template.py
@@ -7,9 +7,6 @@ class ProductTemplate(models.Model):
     
     multi_uom_price_id = fields.One2many('product.multi.uom.price', 'product_id', string="UOM Price")
     category_id = fields.Many2one(related='uom_id.category_id')
-    selected_uom_ids = fields.Many2many(comodel_name="product.multi.uom.price", string="Uom Ids", compute='_get_all_uom_id', store=True)
-
-
 
 
 class SaleOrderLine(models.Model):
