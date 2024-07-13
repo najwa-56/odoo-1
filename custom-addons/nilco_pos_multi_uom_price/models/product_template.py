6b88f59d1fb73134f671c8f03e7fbcf2fc6ba5ac
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-13 16:50:49 +0300

    qq

--- custom-addons/nilco_pos_multi_uom_price/models/product_template.py
+++ custom-addons/nilco_pos_multi_uom_price/models/product_template.py
@@ -31,43 +31,7 @@ class SaleOrderLine(models.Model):
     def sales_multi_uom_id_change(self):
         self.ensure_one()
         if self.sales_multi_uom_id:
-            self.update({"product_uom_qty": self.sales_multi_uom_id.qty})
             domain = {'product_uom': [('id', '=', self.sales_multi_uom_id.uom_id.id)]}
             return {'domain': domain}
-
-    @api.onchange('sales_multi_uom_id','product_uom', 'product_uom_qty')
-    def product_uom_change(self):
-        if not self.product_uom or not self.product_id:
-            self.price_unit = 0.0
-            return
-        if self.sales_multi_uom_id:
-            if self.sales_multi_uom_id:
-                values = {
-                    "product_uom": self.sales_multi_uom_id.uom_id.id,
-                }
-            self.update(values)
-            if self.order_id.partner_id:
-                context_partner = dict(self.env.context, partner_id=self.order_id.partner_id.id)
-                pricelist_context = dict(context_partner, uom=False, date=self.order_id.date_order)
-                price, rule_id = self.order_id.pricelist_id.with_context(pricelist_context)._get_product_price_rule12(
-                    product=self.product_id, quantity=self.sales_multi_uom_id.qty or 1.0,
-                    pro_price=self.sales_multi_uom_id.price, compute_price=False)
-                self.price_unit = self.env['account.tax']._fix_tax_included_price_company(price,
-                                                                                          self.product_id.taxes_id,
-                                                                                          self.tax_id, self.company_id)
-        else:
-            if self.order_id.pricelist_id and self.order_id.partner_id:
-                product = self.product_id.with_context(
-                    lang=self.order_id.partner_id.lang,
-                    partner=self.order_id.partner_id,
-                    quantity=self.product_uom_qty,
-                    date=self.order_id.date_order,
-                    pricelist=self.order_id.pricelist_id.id,
-                    uom=self.product_uom.id,
-                    fiscal_position=self.env.context.get('fiscal_position')
-                )
-                self.price_unit = self.env['account.tax']._fix_tax_included_price_company(
-                    self._get_display_price(), product.taxes_id, self.tax_id, self.company_id)
-
 
 
