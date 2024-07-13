385cb828d07452ffb8c9f4d77454666a2b772a95
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-13 06:28:28 +0300

    1

--- custom-addons/nilco_pos_multi_uom_price/models/product_template.py
+++ custom-addons/nilco_pos_multi_uom_price/models/product_template.py
@@ -13,3 +13,14 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     multi_uom_price_id = fields.One2many('product.multi.uom.price', 'product_id', string="UOM Price")
+    multi_uom_price_info = fields.Char(string='UOM Prices', compute='_compute_multi_uom_price_info')
+
+    @api.depends('product_id')
+    def _compute_multi_uom_price_info(self):
+        for line in self:
+            if line.product_id:
+                uom_prices = line.product_id.multi_uom_price_id
+                prices_info = ', '.join([f"{uom.uom_id.name}: {uom.price}" for uom in uom_prices])
+                line.multi_uom_price_info = prices_info
+            else:
+                line.multi_uom_price_info = ''
