368747278e6a5497d757c60a1efcd4bb050fbeb0
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-13 07:43:44 +0300

    Update product_template.py

--- custom-addons/nilco_pos_multi_uom_price/models/product_template.py
+++ custom-addons/nilco_pos_multi_uom_price/models/product_template.py
@@ -15,21 +15,32 @@ class SaleOrderLine(models.Model):
     available_uoms = fields.Many2many('uom.uom', compute='_compute_available_uoms')
     selected_uom_price = fields.Float(compute='_compute_selected_uom_price')
     product_uom = fields.Many2one('uom.uom', string='Unit of Measure',
-                                  domain=lambda self: [('id', 'in', self.available_uoms.ids)])
+                                  domain=lambda self: [('id', 'in', self._get_available_uom_ids())])
 
     @api.depends('product_id')
     def _compute_available_uoms(self):
-        uom_model = self.env['uom.uom']
-        all_uoms = uom_model.search([])
         for line in self:
-            line.available_uoms = all_uoms
+            if line.product_id:
+                line.available_uoms = line.product_id.multi_uom_price_id.mapped('uom_id')
+            else:
+                line.available_uoms = self.env['uom.uom'].browse([])
 
-    @api.onchange('product_uom')
-    def _onchange_product_uom(self):
+    @api.depends('product_id', 'product_uom')
+    def _compute_selected_uom_price(self):
         for line in self:
             if line.product_id and line.product_uom:
                 uom_price = line.product_id.multi_uom_price_id.filtered(lambda uom: uom.uom_id == line.product_uom)
                 if uom_price:
-                    line.price_unit = uom_price[0].price  # Use the first matched price
+                    line.selected_uom_price = uom_price[0].price
                 else:
-                    line.price_unit = 0.0
+                    line.selected_uom_price = 0.0
+            else:
+                line.selected_uom_price = 0.0
+
+    @api.model
+    def _get_available_uom_ids(self):
+        lines = self.search([])
+        available_uom_ids = []
+        for line in lines:
+            available_uom_ids.append(line.available_uoms.id)
+            available_uom_ids.append(line.product_uom.id)
