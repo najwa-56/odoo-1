91786b119c8919786c4265ee4cd7fef1abef22ce
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-13 17:37:51 +0300

    1111

--- custom-addons/nilco_pos_multi_uom_price/models/product_template.py
+++ custom-addons/nilco_pos_multi_uom_price/models/product_template.py
@@ -10,28 +10,8 @@ class ProductTemplate(models.Model):
     selected_uom_ids = fields.Many2many(comodel_name="product.multi.uom.price", string="Uom Ids", compute='_get_all_uom_id', store=True)
 
 
-    @api.depends('multi_uom_price_id')
-    def _get_all_uom_id(self):
-        for record in self:
-            if record.multi_uom_price_id:
-                record.selected_uom_ids = self.env['product.multi.uom.price'].browse(record.multi_uom_price_id.ids)
-            else:
-                record.selected_uom_ids = []
-
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
-
-    selected_uom_ids = fields.Many2many(string="Uom Ids", related='product_id.selected_uom_ids')
-
-    sales_multi_uom_id = fields.Many2one("product.multi.uom.price", string="Cust UOM", domain="[('id', 'in', selected_uom_ids)]")
-
-    @api.onchange('sales_multi_uom_id')
-    def sales_multi_uom_id_change(self):
-        self.ensure_one()
-        if self.sales_multi_uom_id:
-            domain = {'product_uom': [('id', '=', self.sales_multi_uom_id.uom_id.id)]}
-            return {'domain': domain}
-
 
