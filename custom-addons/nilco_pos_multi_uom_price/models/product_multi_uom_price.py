9c12ee75258becd5ab3aaa5de8d3468686c17bf9
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-15 18:49:07 +0300

    2

--- custom-addons/nilco_pos_multi_uom_price/models/product_multi_uom_price.py
+++ custom-addons/nilco_pos_multi_uom_price/models/product_multi_uom_price.py
@@ -8,7 +8,8 @@ class multi_uom(models.Model):
     product_id = fields.Many2one('product.template',string= 'Product')#,required=True
     category_id = fields.Many2one(related='product_id.uom_id.category_id')
     uom_id = fields.Many2one('uom.uom', string="Unit of Measure", domain="[('category_id', '=', category_id)]")#,required=True
-    price = fields.Float(string='Price',required=True,digits='Product Price')   
+    price = fields.Float(string='Price',required=True,digits='Product Price')
+    cost = fields.Float(string='Cost',required=True,digits='Product Cost')
    # _sql_constraints = [
      #   ('product_multi_uom_price_uniq',
       #   'UNIQUE (product_id,uom_id)',
