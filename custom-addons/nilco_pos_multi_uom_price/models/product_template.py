184a47cd25522386dc47b591ff7a2d2065d8f899
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-15 17:17:30 +0300

    Update product_template.py

--- custom-addons/nilco_pos_multi_uom_price/models/product_template.py
+++ custom-addons/nilco_pos_multi_uom_price/models/product_template.py
@@ -68,3 +68,62 @@ class SaleOrderLine(models.Model):
                 self.price_unit = self.env['account.tax']._fix_tax_included_price_company(
                     self._get_display_price(), product.taxes_id, self.tax_id, self.company_id)
 
+
+class Pricelist(models.Model):
+    _inherit = "product.pricelist"
+
+    def _compute_price_rule12(
+            self, products, quantity, currency=None, uom=None, date=False, compute_price=True, pro_price=0.0,
+            **kwargs
+    ):
+        self and self.ensure_one()  # self is at most one record
+
+        currency = currency or self.currency_id or self.env.company.currency_id
+        currency.ensure_one()
+
+        if not products:
+            return {}
+
+        if not date:
+            # Used to fetch pricelist rules and currency rates
+            date = fields.Datetime.now()
+
+        # Fetch all rules potentially matching specified products/templates/categories and date
+        rules = self._get_applicable_rules(products, date, **kwargs)
+
+        results = {}
+        price = pro_price
+        for product in products:
+            suitable_rule = self.env['product.pricelist.item']
+
+            product_uom = product.uom_id
+            target_uom = uom or product_uom  # If no uom is specified, fall back on the product uom
+
+            # Compute quantity in product uom because pricelist rules are specified
+            # w.r.t product default UoM (min_quantity, price_surchage, ...)
+            if target_uom != product_uom:
+                qty_in_product_uom = target_uom._compute_quantity(
+                    quantity, product_uom, raise_if_failure=False
+                )
+            else:
+                qty_in_product_uom = quantity
+
+            for rule in rules:
+                if rule._is_applicable_for(product, qty_in_product_uom):
+                    suitable_rule = rule
+                    break
+
+            if compute_price:
+                price = suitable_rule._compute_price(
+                    product, quantity, target_uom, date=date, currency=currency)
+            else:
+                # Skip price computation when only the rule is requested.
+                price = pro_price
+            results[product.id] = (price, suitable_rule.id)
+        return results
+
+
+    def _get_product_price_rule12(self,product,*args, **kwargs):
+
+        self and self.ensure_one()  # self is at most one record
+        return self._compute_price_rule12(product,*args, **kwargs)[product.id]
