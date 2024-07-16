90105bc8d1c0a12c91fd17ff917abdbe374f4e4d
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-16 11:56:35 +0300

    Update purchase.py

--- custom-addons/nilco_pos_multi_uom_price/models/purchase.py
+++ custom-addons/nilco_pos_multi_uom_price/models/purchase.py
@@ -9,6 +9,15 @@ class PurchaseOrderLine(models.Model):
                                             domain="[('id', 'in', selected_uom_ids)]")
     purchase_multi_uom_cost = fields.Float(string="UOM Cost", related='purchase_multi_uom_id.cost')
 
+    @api.depends('purchase_multi_uom_cost')
+    def _compute_price_unit(self):
+        for record in self:
+            if record.purchase_multi_uom_cost:
+                record.price_unit = record.purchase_multi_uom_cost
+
+    price_unit = fields.Float(string='Unit Price', compute='_compute_price_unit', store=True)
+
+
 
     @api.onchange('purchase_multi_uom_id')
     def purchase_multi_uom_id_change(self):
@@ -30,21 +39,4 @@ class PurchaseOrderLine(models.Model):
             }
             self.update(values)
 
-    @api.onchange('purchase_multi_uom_id')
-    def _onchange_purchase_multi_uom_id(self):
-        for record in self:
-            if record.purchase_multi_uom_cost:
-                record.price_unit = record.purchase_multi_uom_cost
-
-    @api.model
-    def create(self, vals):
-        if 'purchase_multi_uom_id' in vals:
-            multi_uom = self.env['product.multi.uom.price'].browse(vals['purchase_multi_uom_id'])
-            vals['price_unit'] = multi_uom.cost
-        return super(PurchaseOrderLine, self).create(vals)
-
-    def write(self, vals):
-        if 'purchase_multi_uom_id' in vals:
-            multi_uom = self.env['product.multi.uom.price'].browse(vals['purchase_multi_uom_id'])
-            vals['price_unit'] = multi_uom.cost
-        return super(PurchaseOrderLine, self).write(vals)
+    
