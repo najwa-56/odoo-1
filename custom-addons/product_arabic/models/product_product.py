d8a340fb3312a1b08f5e3e8267b2621e95c3e1a7
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-20 14:36:04 +0300

    2

--- custom-addons/product_arabic/models/product_product.py
+++ custom-addons/product_arabic/models/product_product.py
@@ -33,3 +33,8 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     product_arabic = fields.Char(related='product_id.product_tmpl_id.product_arabic', string='Arabic Name', store=True, readonly=True)
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    product_arabic = fields.Char(related='product_id.product_tmpl_id.product_arabic', string='Arabic Name', store=True, readonly=True)
