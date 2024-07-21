9954f35f37a61ce25829f75bfbdd771fc53a089d
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-21 16:55:54 +0300

    Update ProductCard.js

--- custom-addons/product_arabic/static/src/js/ProductCard.js
+++ custom-addons/product_arabic/static/src/js/ProductCard.js
@@ -4,32 +4,25 @@ import { Component } from "@odoo/owl";
 import { ProductCard } from "@point_of_sale/app/generic_components/product_card/product_card";
 import { patch } from "@web/core/utils/patch";
 
+//Patched the ProductCard for defining that clickMagnifyProduct is function
+patch(ProductCard.prototype, {
+    //Supering setup() function
+    setup() {
+        super.setup();
+    },
+});
 ProductCard.props = {
-    class: { type: String, optional: true },
-    name: { type: String },
-    productArabic: { type: String, optional: true },
-    productId: { type: Number },
-    price: { type: String },
-    imageUrl: { type: String },
-    productInfo: { type: Boolean, optional: true },
-    onClick: { type: Function, optional: true },
-    onProductInfoClick: { type: Function, optional: true },
+        class: { String, optional: true },
+        name: String,
+        productArabic: {String, null},
+        productId: Number,
+        price: String,
+        imageUrl: String,
+        productInfo: { Boolean, optional: true },
+        onClick: { type: Function, optional: true },
+        onProductInfoClick: { type: Function, optional: true },
 };
 
-// Example of passing props to ProductCard
-const productProps = {
-    class: 'product-card',
-    name: 'Sample Product',
-    productArabic: null, // Passing null for productArabic
-    productId: 1,
-    price: '10.00',
-    imageUrl: 'path/to/image',
-    productInfo: true,
-    onClick: () => console.log('Product clicked'),
-    onProductInfoClick: () => console.log('Product info clicked'),
-};
 
-// Using the ProductCard component with the productProps
-<ProductCard {...productProps} />
 
 
