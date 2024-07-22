1bcb73edc46c8409ff592962e0db25d4e5b8349c
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-22 14:32:04 +0300

    2

--- custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
+++ custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
@@ -12,7 +12,13 @@ class Inheritmulti_uom(models.Model):
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    multi_uom_price_barcode = fields.Char(related='multi_uom_price_id.barcode', string='Barcode')
+    multi_uom_price_barcode = fields.Char(
+        string='Barcode',
+        compute='_compute_multi_uom_price_barcode',
+        store=True
+    )
 
- 
-    
+    @api.depends('multi_uom_price_id')
+    def _compute_multi_uom_price_barcode(self):
+        for record in self:
+            record.multi_uom_price_barcode = record.multi_uom_price_id.barcode if record.multi_uom_price_id else ''
