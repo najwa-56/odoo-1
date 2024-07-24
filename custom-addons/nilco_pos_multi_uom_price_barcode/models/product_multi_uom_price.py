cb8409a2c6d4e8d01a51962e61e52d2c7133784b
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-24 14:44:50 +0300

    3

--- custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
+++ custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
@@ -29,12 +29,12 @@ class ProductTemplate(models.Model):
                 record.multi_uom_price_barcode = ''
 
 
-   # @api.model
-  #  def search_product_by_barcode(self, barcode):
+    @api.model
+    def search_product_by_barcode(self, barcode):
         # Search for the `product.multi.uom.price` record with the given barcode
-    #    uom_price_records = self.env['product.multi.uom.price'].search([('barcode', '=', barcode)])
-      #  if uom_price_records:
+        uom_price_records = self.env['product.multi.uom.price'].search([('barcode', '=', barcode)])
+        if uom_price_records:
             # Return the product.template linked to the found `product.multi.uom.price` records
-       #    return uom_price_records.mapped('product_id.product_tmpl_id')
-      #  return self.env['product.template']
+            return uom_price_records.mapped('product_id.product_tmpl_id')
+        return self.env['product.template']
 
