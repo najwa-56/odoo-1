e3f10d6316df3f20cff3a66792f8c94d3eef361f
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-24 15:08:49 +0300

    Update product_multi_uom_price.py

--- custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
+++ custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class Inheritmulti_uom(models.Model):
@@ -38,3 +39,37 @@ class ProductTemplate(models.Model):
             return uom_price_records.mapped('product_id.product_tmpl_id')
         return self.env['product.template']
 
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    def _search_product_by_barcode(self, barcode):
+        # Call the search_product_by_barcode method from the product.template model
+        product_template = self.env['product.template'].search_product_by_barcode(barcode)
+        if product_template:
+            return product_template[0]  # Assuming you only want one result
+        return False
+
+    @api.model
+    def create_or_update_order_line_from_barcode(self, order_id, barcode, qty=1.0):
+        # Find the product template using the barcode
+        product_template = self._search_product_by_barcode(barcode)
+
+        if product_template:
+            # Check if the product already exists in the order
+            existing_line = self.search(
+                [('order_id', '=', order_id), ('product_id.product_tmpl_id', '=', product_template.id)])
+
+            if existing_line:
+                # Update the existing line
+                existing_line.write({'product_uom_qty': existing_line.product_uom_qty + qty})
+            else:
+                # Create a new order line
+                self.create({
+                    'order_id': order_id,
+                    'product_id': product_template.product_variant_id.id,
+                    'product_uom_qty': qty,
+                    'price_unit': product_template.product_variant_id.lst_price,
+                })
+        else:
+            raise UserError("Product with barcode {} not found.".format(barcode))
