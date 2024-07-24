8e211a52ee65769247faa863a3a20a00be8d3660
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-24 17:20:26 +0300

    Update res_config_inherit2.py

--- custom-addons/nilco_pos_multi_uom_price_barcode/models/res_config_inherit2.py
+++ custom-addons/nilco_pos_multi_uom_price_barcode/models/res_config_inherit2.py
@@ -5,5 +5,4 @@ from odoo import api, fields, models, _
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    multi_barcode_for_product = fields.Boolean(string="Multi Barcode For Product")
 
