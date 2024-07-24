67b5cb2e53c03532b4d91364eefb5a2a486272d2
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-25 00:36:04 +0300

    Update product_multi_uom_price.py

--- custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
+++ custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
@@ -46,79 +46,99 @@ class ProductTemplate(models.Model):
                 record.multi_uom_price_barcode = ''
 
 
-
 class ProductInherit(models.Model):
     _inherit = 'product.product'
 
-    product_barcode = fields.One2many('product.multi.uom.price', 'product_id', string='Product Multi Barcodes')
-
     @api.model
     def _name_search(self, name, domain=None, operator='ilike', limit=None, order=None):
-        active_model = self._context.get('active_model') or self._inherit
         company_id = self.env.user.company_id
-
         if company_id.multi_barcode_for_product:
             if not domain:
                 domain = []
-            if name:
-                positive_operators = ['=', 'ilike', '=ilike', 'like', '=like']
-                product_ids = []
 
+            positive_operators = ['=', 'ilike', '=ilike', 'like', '=like']
+            product_ids = []
+            model_name = []
+
+            if name:
                 if operator in positive_operators:
-                    # Search by default_code
+                    # Search by default code
                     product_ids = list(self._search([('default_code', '=', name)] + domain, limit=limit, order=order))
 
-                    # If no results, search by barcode
                     if not product_ids:
+                        # Search by barcode
                         product_ids = list(self._search([('barcode', '=', name)] + domain, limit=limit, order=order))
 
-                    # Handle multi_uom_price_id barcodes
-                    if not product_ids:
-                        product_barcode_ids = self.env['product.multi.uom.price']._search([('barcode', operator, name)])
-                        if product_barcode_ids:
-                            product_ids = list(self._search([
-                                '|', ('barcode', '=', name), ('multi_uom_price_id.barcode', '=', name)
-                            ] + domain, limit=limit, order=order))
+                        if not product_ids:
+                            # Determine the model name
+                            create_date_model = self._context.get('create_date_model')
+                            model_mapping = {
+                                "sale.order.line": "Sales Order",
+                                "purchase.order.line": "Purchase Order",
+                                "account.move.line": "Account",
+                                "stock.scrap": "Scrap",
+                                "stock.move": "Transfer",
+                                "stock.quant": "Quants",
+                                "stock.warehouse.orderpoint": "Minimum Inventory Rule"
+                            }
+                            model_name = model_mapping.get(create_date_model)
 
-                # Additional searches if no results found
-                if not product_ids and operator not in expression.NEGATIVE_TERM_OPERATORS:
-                    product_ids = list(self._search(domain + [('default_code', operator, name)], limit=limit))
-                    if not limit or len(product_ids) < limit:
-                        limit2 = (limit - len(product_ids)) if limit else False
-                        product2_ids = self._search(
-                            domain + [('name', operator, name), ('id', 'not in', product_ids)], limit=limit2, order=order)
-                        product_ids.extend(product2_ids)
+                            # Search for multi UOM barcodes
+                            product_barcode_ids = self.env['product.multi.uom.price']._search([
+                                ('barcode', operator, name),
+                                ('model_ids.name', "=", model_name)
+                            ])
+                            if product_barcode_ids:
+                                product_ids = list(self._search([
+                                                                    '|',
+                                                                    ('product_barcode', 'in', product_barcode_ids),
+                                                                    ('product_tmpl_id.product_barcode', 'in',
+                                                                     product_barcode_ids)
+                                                                ] + domain, limit=limit, order=order))
 
-                elif not product_ids and operator in expression.NEGATIVE_TERM_OPERATORS:
-                    domain_add = expression.OR([
-                        ['&', ('default_code', operator, name), ('name', operator, name)],
-                        ['&', ('default_code', '=', False), ('name', operator, name)],
-                    ])
-                    domain_add = expression.AND([domain, domain_add])
-                    product_ids = list(self._search(domain_add, limit=limit, order=order))
+            if not product_ids and operator not in expression.NEGATIVE_TERM_OPERATORS:
+                # Additional search to include names
+                product_ids = list(self._search(domain + [('default_code', operator, name)], limit=limit))
+                if not limit or len(product_ids) < limit:
+                    limit2 = (limit - len(product_ids)) if limit else False
+                    product2_ids = self._search(
+                        domain + [('name', operator, name), ('id', 'not in', product_ids)],
+                        limit=limit2, order=order)
+                    product_ids.extend(product2_ids)
 
-                if not product_ids and operator in positive_operators:
-                    ptrn = re.compile('(\[(.*?)\])')
-                    res = ptrn.search(name)
-                    if res:
-                        product_ids = list(self._search([('default_code', '=', res.group(2))] + domain, limit=limit, order=order))
+            elif not product_ids and operator in expression.NEGATIVE_TERM_OPERATORS:
+                domain_add = expression.OR([
+                    ['&', ('default_code', operator, name), ('name', operator, name)],
+                    ['&', ('default_code', '=', False), ('name', operator, name)],
+                ])
+                domain_add = expression.AND([domain, domain_add])
+                product_ids = list(self._search(domain_add, limit=limit, order=order))
 
-                if not product_ids and self._context.get('partner_id'):
-                    suppliers_ids = self.env['product.supplierinfo']._search([
-                        ('product_name', '=', self._context.get('partner_id')),
-                        '|', ('product_code', operator, name), ('product_name', operator, name)])
-                    if suppliers_ids:
-                        product_ids = self._search([('product_id.seller_ids', 'in', suppliers_ids)], limit=limit, order=order)
+            if not product_ids and operator in positive_operators:
+                ptrn = re.compile(r'\[(.*?)\]')
+                res = ptrn.search(name)
+                if res:
+                    product_ids = list(
+                        self._search([('default_code', '=', res.group(1))] + domain, limit=limit, order=order))
 
-                # Search based on Multi Barcode
+            if not product_ids and self._context.get('partner_id'):
+                suppliers_ids = self.env['product.supplierinfo']._search([
+                    ('product_name', '=', self._context.get('partner_id')),
+                    '|', ('product_code', operator, name), ('product_name', operator, name)])
+                if suppliers_ids:
+                    product_ids = self._search([('product_tmpl_id.seller_ids', 'in', suppliers_ids)], limit=limit,
+                                               order=order)
+
+            # Additional search based on Multi Barcode
+            if not product_ids:
                 product_barcode_ids = self.env['product.multi.uom.price']._search([('barcode', operator, name)])
                 if product_barcode_ids:
                     product_ids = list(self._search([
-                        '|', ('multi_uom_price_id', 'in', product_barcode_ids), ('product_id.multi_uom_price_id', 'in', product_barcode_ids)
+                        '|',
+                        ('product_tmpl_id.product_barcode', 'in', product_barcode_ids)
                     ], limit=limit, order=order))
-            else:
-                product_ids = self._search(domain, limit=limit, order=order)
 
             return product_ids
         else:
+            # Default search behavior if multi-barcode is not enabled
             return super(ProductInherit, self)._name_search(name, domain, operator, limit, order)
