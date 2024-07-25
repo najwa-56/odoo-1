6ff05e476530f1b0a95f0d8a3475cbd3188a5e22
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-25 18:28:13 +0300

    y

--- custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
+++ custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
@@ -49,3 +49,31 @@ class SaleOrderLine(models.Model):
                 self.name = ''
                 self.product_uom_qty = 0.0
 
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    barcode = fields.Char('Barcode')
+
+    @api.onchange('barcode')
+    def _onchange_barcode(self):
+        if self.barcode:
+            # Search for the product by barcode
+            product = self.env['product.product'].search([('multi_uom_price_id.barcode', '=', self.barcode)], limit=1)
+            if product:
+                self.product_id = product
+                self.name = product.name
+                # Set quantity to 1 as default
+                self.product_qty = 1
+
+                # Set purchase_multi_uom_id based on barcode
+                multi_uom = self.env['product.multi.uom.price'].search([('barcode', '=', self.barcode)], limit=1)
+                if multi_uom:
+                    self.purchase_multi_uom_id = multi_uom.id
+                    self.product_uom = multi_uom.uom_id.id
+            else:
+                # Clear product_id and related fields if barcode is not found
+                self.product_id = False
+                self.name = ''
+                self.product_qty = 0.0
+                self.product_uom = False
--- custom-addons/sh_barcode_scanner/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) Softhealer Technologies.
-
-from . import controllers
-from . import models
