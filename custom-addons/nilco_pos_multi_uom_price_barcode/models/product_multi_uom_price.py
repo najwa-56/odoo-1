669814aed9f70b03426c233ce577ae5cef7eb353
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-25 14:21:11 +0300

    Update product_multi_uom_price.py

--- custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
+++ custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
@@ -13,6 +13,9 @@ class Inheritmulti_uom(models.Model):
     barcode = fields.Char('Barcode')
     product_variant_id =fields.Many2one('product.product',related="product_id.product_variant_id",store=True)
     product_variant_count = fields.Integer('Product Variant Count')
+    _sql_constraints = [
+        ('uniq_barcode', 'unique(barcode)', "A barcode can only be assigned to one product !"),
+    ]
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
