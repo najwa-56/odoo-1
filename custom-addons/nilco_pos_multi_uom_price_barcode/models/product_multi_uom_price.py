e0bb4459ba7b2a353fc3054f910a7dea974d4470
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-07-27 09:32:55 +0300

    2

--- custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
+++ custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
@@ -1,4 +1,6 @@
 from odoo import models, fields, api, _
+import re
+from odoo.osv import expression
 
 class Inheritmulti_uom(models.Model):
     _inherit = 'product.multi.uom.price'
@@ -7,22 +9,61 @@ class Inheritmulti_uom(models.Model):
     product_variant_id =fields.Many2one('product.product',related="product_id.product_variant_id",store=True)
     product_variant_count = fields.Integer('Product Variant Count')
 
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
 
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    barcode = fields.Char('Barcode')
 
-    multi_uom_price_barcode = fields.Char(
-        string='Barcode',
-        compute='_compute_multi_uom_price_barcode',
-        store=True
-    )
+    @api.onchange('barcode')
+    def _onchange_barcode(self):
+        if self.barcode:
+            # Search for the product by barcode
+            product = self.env['product.product'].search([('multi_uom_price_id.barcode', '=', self.barcode)], limit=1)
+            if product:
+                self.product_id = product
+                self.name = product.name
 
-    @api.depends('multi_uom_price_id')
-    def _compute_multi_uom_price_barcode(self):
-        for record in self:
-            if record.multi_uom_price_id:
-                # If you want to concatenate all barcodes into one string
-                barcodes = record.multi_uom_price_id.mapped('barcode')
-                record.multi_uom_price_barcode = ', '.join(filter(None, barcodes))
+                self.price_unit = product.list_price
+                # Set quantity to 1 as default
+                self.product_uom_qty = 1
             else:
-                record.multi_uom_price_barcode = ''
+                # Clear product_id if barcode is not found
+                self.product_id = False
+                self.name = ''
+                self.price_unit = 0.0
+                self.product_uom_qty = 0.0
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    barcode = fields.Char('Barcode')
+
+    @api.onchange('barcode')
+    def _onchange_barcode(self):
+        if self.barcode:
+            # Search for the product by barcode
+            product = self.env['product.product'].search([('multi_uom_price_id.barcode', '=', self.barcode)], limit=1)
+            if product:
+                self.product_id = product
+                self.name = product.name
+                self.price_unit = product.list_price
+                # Set quantity to 1 as default
+                self.product_qty = 1
+
+                # Set purchase_multi_uom_id based on barcode
+                multi_uom = self.env['product.multi.uom.price'].search([('barcode', '=', self.barcode)], limit=1)
+                if multi_uom:
+                    self.purchase_multi_uom_id = multi_uom.id
+                    self.product_uom = multi_uom.uom_id.id
+            else:
+                # Clear product_id and related fields if barcode is not found
+                self.product_id = False
+                self.name = ''
+                self.price_unit = 0.0
+                self.product_qty = 0.0
+                self.product_uom = False
+
+
+
+
