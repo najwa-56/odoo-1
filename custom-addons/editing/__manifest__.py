2fc1ccb2c0494ce33ad2d7d1f9fee00dd8a9a2b2
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-03 15:29:08 +0300

    closing-popup

--- custom-addons/editing/__manifest__.py
+++ custom-addons/editing/__manifest__.py
@@ -9,10 +9,15 @@
     'author': 'Najwa and Abeer',
     'summary': "A module for editing purposes",
     'description': """This module provides editing functionality.""",
-    'depends': ['sale_management', 'stock'],
+    'depends': ['sale_management', 'stock','point_of_sale'],
     'data': [
         'views/editing.xml',
     ],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'editing/static/src/**/*',
+        ],
+    },
     'license': 'LGPL-3',
     'installable': True,
     'website': 'https://www.example.com',
