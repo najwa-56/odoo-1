1cbaf2ff7ee45e49824f7f80f94f2228c9fdd9da
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-05 17:18:33 +0300

    3

--- custom-addons/nilco_pos_multi_uom_price/static/src/js/multi_uom_price.js
+++ custom-addons/nilco_pos_multi_uom_price/static/src/js/multi_uom_price.js
@@ -3,7 +3,7 @@ import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product
 import { Component } from "@odoo/owl";
 import { useListener } from "@web/core/utils/hooks";
 import { SelectionPopup } from "@point_of_sale/app/utils/input_popups/selection_popup";
-import { QuantityPopup } from "./path/to/quantity_popup";
+import { QuantityPopup } from "./odoo-1-2/custom-addons/nilco_pos_multi_uom_price/static/src/js/quantity_popup";
 import { _t } from "@web/core/l10n/translation";  // Import _t for translations
 
 export class UOMButton extends Component {
