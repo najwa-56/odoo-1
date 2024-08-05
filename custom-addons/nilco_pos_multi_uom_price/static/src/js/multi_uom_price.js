8aa9391a6c6f5c2757cfde61253e2750e9ea1f68
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-05 09:31:08 +0300

    3

--- custom-addons/nilco_pos_multi_uom_price/static/src/js/multi_uom_price.js
+++ custom-addons/nilco_pos_multi_uom_price/static/src/js/multi_uom_price.js
@@ -34,18 +34,14 @@ export class UOMButton extends Component {
 		       }
 		       const { confirmed, payload: selectedUOM } = await this.env.services.popup.add(
 			            SelectionPopup, {
-			       title: _t('UOM'),
+			       title: 'UOM',
 			       list: uomList,
 		       });
-		       if (confirmed && selectedUOM) {
-                    // Assuming line.set_uom is a method that updates the UOM and price
-                    line.set_uom(selectedUOM.id);
-                    line.price_manually_set = true;
-                    line.set_unit_price(selectedUOM.price);
-
-                    // Optionally, recalculate subtotal and total if needed
-                    line.trigger('change', line);
-                }
+		       if (confirmed) {
+			      line.set_uom({0:selectedUOM.id,1:selectedUOM.name});
+			      line.price_manually_set = true;
+			      line.set_unit_price(selectedUOM.price);
+		       }
 	         }
 	       }
        }
