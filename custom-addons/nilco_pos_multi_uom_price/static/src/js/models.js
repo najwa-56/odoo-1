cbb6e5c23d9cc72e30ec22c94263b2f09c61faf9
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-08 10:07:13 +0300

    Update models.js

--- custom-addons/nilco_pos_multi_uom_price/static/src/js/models.js
+++ custom-addons/nilco_pos_multi_uom_price/static/src/js/models.js
@@ -17,7 +17,41 @@ patch(Order.prototype, {
             orderline.product_uom_id = options.product_uom_id;
 
         }
-    }
+    },
+      set_pricelist(pricelist) {
+        super.setup(...arguments);
+        var self = this;
+        this.pricelist = pricelist;
+
+        const orderlines = this.get_orderlines();
+
+        const lines_to_recompute = orderlines.filter(
+            (line) =>
+                line.price_type === "original" && !(line.comboLines?.length || line.comboParent)
+        );
+        const combo_parent_lines = orderlines.filter(
+            (line) => line.price_type === "original" && line.comboLines?.length
+        );
+        const attributes_prices = {};
+        combo_parent_lines.forEach((parentLine) => {
+            attributes_prices[parentLine.id] = this.compute_child_lines(
+                parentLine.product,
+                parentLine.comboLines.map((childLine) => {
+                    const comboLineCopy = { ...childLine.comboLine };
+                    if (childLine.attribute_value_ids) {
+                        comboLineCopy.configuration = {
+                            attribute_value_ids: childLine.attribute_value_ids,
+                        };
+                    }
+                    return comboLineCopy;
+                }),
+                pricelist
+            );
+        });
+        const combo_children_lines = orderlines.filter(
+            (line) => line.price_type === "original" && line.comboParent
+        );
+    },
 });
 patch(Orderline.prototype, {
     setup(_defaultObj, options) {
@@ -129,45 +163,6 @@ patch(Orderline.prototype, {
             this.order.fix_tax_included_price(this);
         }
         return true;
-    }
-    set_pricelist(pricelist) {
-        var self = this;
-        this.pricelist = pricelist;
-
-        const orderlines = this.get_orderlines();
-
-        const lines_to_recompute = orderlines.filter(
-            (line) =>
-                line.price_type === "original" && !(line.comboLines?.length || line.comboParent)
-        );
-        lines_to_recompute.forEach((line) => {
-            line.set_unit_price(
-                line.product.get_price(self.pricelist, line.get_quantity(), line.get_price_extra())
-            );
-            self.fix_tax_included_price(line);
-        });
-        const combo_parent_lines = orderlines.filter(
-            (line) => line.price_type === "original" && line.comboLines?.length
-        );
-        const attributes_prices = {};
-        combo_parent_lines.forEach((parentLine) => {
-            attributes_prices[parentLine.id] = this.compute_child_lines(
-                parentLine.product,
-                parentLine.comboLines.map((childLine) => {
-                    const comboLineCopy = { ...childLine.comboLine };
-                    if (childLine.attribute_value_ids) {
-                        comboLineCopy.configuration = {
-                            attribute_value_ids: childLine.attribute_value_ids,
-                        };
-                    }
-                    return comboLineCopy;
-                }),
-                pricelist
-            );
-        });
-        const combo_children_lines = orderlines.filter(
-            (line) => line.price_type === "original" && line.comboParent
-        );
     }
 
 });
