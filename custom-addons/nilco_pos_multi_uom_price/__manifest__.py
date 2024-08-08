dc35edcdd8b00c5a085d88d973f6ca1e79927184
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-08 11:26:39 +0300

    Update __manifest__.py

--- custom-addons/nilco_pos_multi_uom_price/__manifest__.py
+++ custom-addons/nilco_pos_multi_uom_price/__manifest__.py
@@ -13,7 +13,7 @@
     'website': "nilcotechnology@gmail.com",
     'currency': 'USD',
     'price': 25.6,
-    'depends': ['point_of_sale','stock','uom'],
+    'depends': ['point_of_sale','stock','uom','base'],
     'data': [
         'security/ir.model.access.csv',
         'views/product_view.xml',
