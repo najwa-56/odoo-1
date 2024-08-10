9d96d92387522661407ef43f7756a4d6e53f82ad
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-10 12:10:37 +0300

    2

--- custom-addons/nilco_pos_multi_uom_price/static/src/js/models.js
+++ custom-addons/nilco_pos_multi_uom_price/static/src/js/models.js
@@ -3,8 +3,8 @@ import { Order, Orderline, Payment } from "@point_of_sale/app/store/models";
 import { patch } from "@web/core/utils/patch";
 import { PosStore } from "@point_of_sale/app/store/pos_store";
 import { _t } from '@web/core/l10n/translation';
-import { fetchUserGroups } from "./utils"; // Adjust the import path as necessary
-import { ErrorPopup } from "@point_of_sale/app/errors/popups/error_popup";
+import { ajax } from '@web/core/ajax';
+import { fetchUserGroups } from "./utils"; /import { ErrorPopup } from "@point_of_sale/app/errors/popups/error_popup";
 import { parseFloat as oParseFloat } from "@web/views/fields/parsers";
 import {
     formatFloat,
