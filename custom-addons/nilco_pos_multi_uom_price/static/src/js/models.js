54e082464cce1e212e335021d6fae25537609ab7
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-10 14:08:23 +0300

    Update models.js

--- custom-addons/nilco_pos_multi_uom_price/static/src/js/models.js
+++ custom-addons/nilco_pos_multi_uom_price/static/src/js/models.js
@@ -190,14 +190,14 @@ patch(PosStore.prototype, {
         await super._processData(...arguments);
             this.product_uom_price = loadedData['product.multi.uom.price'];
          try {
-            this.zero1 = await this.user_groups(); // Store the zero value in PosStore
+            this.zero1 = await this.user_groups2(); // Store the zero value in PosStore
             console.log('Stored zero1:', this.zero1); // Check the value stored
         } catch (error) {
             console.error('Error storing zero1:', error);
         }// Store the zero value in PosStore
     },
 
-    async user_groups(){
+    async user_groups2(){
         await this.orm.call(
             "pos.session",
             "get_user_groups2",
