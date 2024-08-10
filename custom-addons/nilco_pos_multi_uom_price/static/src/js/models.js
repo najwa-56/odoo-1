591821e36c15502a4ac5d0f432a6e62ecbb3e3ce
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-10 13:47:25 +0300

    3

--- custom-addons/nilco_pos_multi_uom_price/static/src/js/models.js
+++ custom-addons/nilco_pos_multi_uom_price/static/src/js/models.js
@@ -59,11 +59,15 @@ patch(Orderline.prototype, {
     setup(_defaultObj, options) {
         super.setup(...arguments);
         this.product_uom_id = this.product.default_uom_id || this.product_uom_id || this.product.uom_id;
-         const posStore = this.pos; // Assuming `this.pos` is an instance of PosStore
+         // Access the PosStore instance to get zero1
+        const posStore = this.pos; // Ensure this is the correct reference to PosStore
         if (posStore && posStore.zero1) {
             console.log('zero1 value:', posStore.zero1);
             // You can use zero1 here as needed
+        } else {
+            console.error('zero1 is not defined in PosStore');
         }
+
 
     },
 
