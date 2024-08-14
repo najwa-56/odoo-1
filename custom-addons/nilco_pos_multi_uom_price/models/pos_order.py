d7269668a0cc4d90a897a7eef84d936d9f8b070a
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-14 12:44:13 +0300

    3

--- custom-addons/nilco_pos_multi_uom_price/models/pos_order.py
+++ custom-addons/nilco_pos_multi_uom_price/models/pos_order.py
@@ -14,11 +14,23 @@ class PosOrderLine(models.Model):
     #add field Ratio#####
     Ratio = fields.Float("Ratio", compute="_compute_ratio",
                          store=False)  # Ratio field  # Related field to the ratio in uom.uom
-    selected_uom_ids = fields.Many2many(string="Uom Ids", related='product_id.selected_uom_ids')
 
     sales_multi_uom_id = fields.Many2one("product.multi.uom.price", string="Cust UOM",
-                                         domain="[('id', 'in', selected_uom_ids)]")
+                                         domain=lambda self: self._get_sales_multi_uom_domain())
     name_field = fields.Char(String="Name_uom", related='sales_multi_uom_id.name_field')
+
+    @api.depends('product_uom_id')
+    def _get_sales_multi_uom_domain(self):
+        # Compute the domain for sales_multi_uom_id based on product_uom_id
+        uom_id = self.product_uom_id.id
+        if uom_id:
+            return [('uom_id', '=', uom_id)]
+        return []
+
+    @api.depends('product_uom_id')
+    def _compute_ratio(self):
+        # Your logic to compute ratio
+        pass
 #Edit----#
 
 
