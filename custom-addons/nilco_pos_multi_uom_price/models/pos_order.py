3748e0a4421275ee8681a5eaa996e985a7072789
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-14 17:34:17 +0300

    3

--- custom-addons/nilco_pos_multi_uom_price/models/pos_order.py
+++ custom-addons/nilco_pos_multi_uom_price/models/pos_order.py
@@ -15,18 +15,6 @@ class PosOrderLine(models.Model):
     Ratio = fields.Float("Ratio", compute="_compute_ratio",
                          store=False)  # Ratio field  # Related field to the ratio in uom.uom
 
-    name_field = fields.Char(string="Name Field")
-
-    @api.onchange('product_id')
-    def _onchange_product_id(self):
-        super()._onchange_product_id()
-        if self.product_id:
-            # Get the related multi_uom_price record
-            multi_uom_price = self.env['product.multi.uom.price'].search([
-                ('product_id', '=', self.product_id.id),
-                ('uom_id', '=', self.product_uom_id.id)
-            ], limit=1)
-            self.name_field = multi_uom_price.name_field if multi_uom_price else ''
 #Edit----#
 
 
@@ -70,7 +58,6 @@ class PosOrderLine(models.Model):
 
     def _export_for_ui(self, orderline):
         res = super()._export_for_ui(orderline)
-        res.update({'product_uom_id': orderline.product_uom_id.id,
-                    'name_field': orderline.name_field,})
+        res.update({'product_uom_id': orderline.product_uom_id.id})
 
         return res
