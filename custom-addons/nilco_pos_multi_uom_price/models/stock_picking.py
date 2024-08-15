bae14e38bf728a33903bec3bc9250bdb429cb814
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-15 17:41:16 +0300

    Update stock_picking.py

--- custom-addons/nilco_pos_multi_uom_price/models/stock_picking.py
+++ custom-addons/nilco_pos_multi_uom_price/models/stock_picking.py
@@ -10,18 +10,29 @@ class StockPicking(models.Model):
     _inherit='stock.picking'
 
     def _prepare_stock_move_vals(self, first_line, order_lines):
-        res = super()._prepare_stock_move_vals(first_line, order_lines)
-        res.update({'product_uom': first_line.product_uom_id.id})
-        return res
+        # Aggregate quantities for the given UOM and product
+        total_qty = sum(line.qty for line in order_lines)
+        return {
+            'product_id': first_line.product_id.id,
+            'product_uom': first_line.product_uom_id.id,
+            'product_uom_qty': total_qty,
+            'name': first_line.product_id.name,
+            'location_id': self.picking_type_id.default_location_src_id.id,
+            'location_dest_id': self.picking_type_id.default_location_dest_id.id,
+        }
 
     def _create_move_from_pos_order_lines(self, lines):
         self.ensure_one()
 
-        lines_by_product = groupby(sorted(lines, key=lambda l: (l.product_id.id,l.product_uom_id.id)), key=lambda l: (l.product_id.id,l.product_uom_id.id))
+        # Group lines by product and UOM
+        lines_by_product = groupby(sorted(lines, key=lambda l: (l.product_id.id, l.product_uom_id.id)),
+                                   key=lambda l: (l.product_id.id, l.product_uom_id.id))
         move_vals = []
+
         for dummy, olines in lines_by_product:
             order_lines = self.env['pos.order.line'].concat(*olines)
             move_vals.append(self._prepare_stock_move_vals(order_lines[0], order_lines))
+
         moves = self.env['stock.move'].create(move_vals)
         confirmed_moves = moves._action_confirm()
         confirmed_moves._add_mls_related_to_order(lines, are_qties_done=True)
