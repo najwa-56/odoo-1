92eb5c39bd5240e88c7e70cf323e17ff544d9423
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-20 08:47:07 +0300

    2

--- custom-addons/nilco_pos_multi_uom_price/static/src/js/multi_uom_price.js
+++ custom-addons/nilco_pos_multi_uom_price/static/src/js/multi_uom_price.js
@@ -43,6 +43,7 @@ export class UOMButton extends Component {
 			      line.price_manually_set = true;
 			      line.set_unit_price(selectedUOM.price);
 			      line.set_uom_name(selectedUOM.name_field);
+			      console.log(selectedUOM.name_field)
 
 
 		       }
