69b66727db23de6a54eb0b80a1c7c38691726b45
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-21 07:09:51 +0300

    Update pos_session.py

--- custom-addons/nilco_pos_multi_uom_price/models/pos_session.py
+++ custom-addons/nilco_pos_multi_uom_price/models/pos_session.py
@@ -46,7 +46,7 @@ class PosSession(models.Model):
                 'fields': [
                     'display_name', 'lst_price', 'standard_price', 'categ_id', 'pos_categ_ids', 'taxes_id', 'barcode',
                     'default_code', 'to_weight', 'uom_id', 'description_sale', 'description', 'product_tmpl_id', 'tracking',
-                    'write_date', 'available_in_pos', 'attribute_line_ids', 'active', 'image_128', 'combo_ids','name_field'],
+                    'write_date', 'available_in_pos', 'attribute_line_ids', 'active', 'image_128', 'combo_ids'],
                 'order': 'sequence,default_code,name',
             },
             'context': {'display_default_code': False},
