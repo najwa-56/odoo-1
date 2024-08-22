60e38cec6456aa19ddbfd14cd3ce588895fd91e5
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-22 09:19:47 +0300

    Update pos_order.py

--- custom-addons/nilco_pos_multi_uom_price/models/pos_order.py
+++ custom-addons/nilco_pos_multi_uom_price/models/pos_order.py
@@ -77,42 +77,52 @@ class PosOrderLine(models.Model):
 class PosOrder(models.Model):
     _inherit = "pos.order"
 
+    name_field = fields.Char(string="Name Field", store=True)
+
     @api.model
     def _get_invoice_lines_values(self, line_values, pos_order_line):
         return {
-
-            'name_field': line_values['name_field'],
-
+            'name_field': line_values.get('name_field', 'Default Value'),  # Replace 'Default Value' as appropriate
         }
 
     def _prepare_invoice_lines(self):
-        """ Prepare a list of orm commands containing the dictionaries to fill the
-        'invoice_line_ids' field when creating an invoice.
-
-        :return: A list of Command.create to fill 'invoice_line_ids' when calling account.move.create.
-        """
         sign = 1 if self.amount_total >= 0 else -1
         line_values_list = self._prepare_tax_base_line_values(sign=sign)
         invoice_lines = []
         for line_values in line_values_list:
             line = line_values['record']
+            # Ensure name_field is included in line_values
+            line_values.update({'name_field': line.name_field})  # Ensure this line_field is set correctly
             invoice_lines_values = self._get_invoice_lines_values(line_values, line)
             invoice_lines.append((0, None, invoice_lines_values))
-            if line.order_id.pricelist_id.discount_policy == 'without_discount' and float_compare(
-                    line.price_subtotal_incl, line.product_id.lst_price * line.qty,
-                    precision_rounding=self.currency_id.rounding) < 0:
-                invoice_lines.append((0, None, {
-                    'name': _('Price discount from %s -> %s',
-                              float_repr(line.product_id.lst_price * line.qty, self.currency_id.decimal_places),
-                              float_repr(line.price_subtotal_incl, self.currency_id.decimal_places)),
-                    'display_type': 'line_note',
-                }))
-            if line.customer_note:
-                invoice_lines.append((0, None, {
-                    'name': line.customer_note,
-                    'display_type': 'line_note',
-                }))
-
+            # Other conditions...
         return invoice_lines
 
 
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+
+
+def _get_invoiced_lot_values(self):
+        self.ensure_one()
+
+        lot_values = super(AccountMove, self)._get_invoiced_lot_values()
+
+        if self.state == 'draft':
+            return lot_values
+
+        # user may not have access to POS orders, but it's ok if they have
+        # access to the invoice
+        for order in self.sudo().pos_order_ids:
+            for line in order.lines:
+                lots = line.pack_lot_ids or False
+                if lots:
+                    for lot in lots:
+                        lot_values.append({
+
+                            'name_field': line.name_field,
+
+                        })
+
+        return lot_values
