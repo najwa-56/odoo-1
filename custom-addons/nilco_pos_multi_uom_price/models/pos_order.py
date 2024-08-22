fa36bb17f85e63cf8d8b321b934443ee9519cc52
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-22 09:33:14 +0300

    Update pos_order.py

--- custom-addons/nilco_pos_multi_uom_price/models/pos_order.py
+++ custom-addons/nilco_pos_multi_uom_price/models/pos_order.py
@@ -99,30 +99,3 @@ class PosOrder(models.Model):
         return invoice_lines
 
 
-class AccountMove(models.Model):
-    _inherit = 'account.move'
-
-
-
-def _get_invoiced_lot_values(self):
-        self.ensure_one()
-
-        lot_values = super(AccountMove, self)._get_invoiced_lot_values()
-
-        if self.state == 'draft':
-            return lot_values
-
-        # user may not have access to POS orders, but it's ok if they have
-        # access to the invoice
-        for order in self.sudo().pos_order_ids:
-            for line in order.lines:
-                lots = line.pack_lot_ids or False
-                if lots:
-                    for lot in lots:
-                        lot_values.append({
-
-                            'name_field': line.name_field,
-
-                        })
-
-        return lot_values
