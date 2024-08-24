adc834d72bb2be89621695c38f319546c489da7c
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-24 12:54:04 +0300

    Update pos_order.py

--- custom-addons/nilco_pos_multi_uom_price/models/pos_order.py
+++ custom-addons/nilco_pos_multi_uom_price/models/pos_order.py
@@ -29,7 +29,7 @@ class PosOrderLine(models.Model):
 
         # Update the result with custom fields
         invoice_line_vals.update({
-            'product_uom_id': self.product_uom_id.name,
+            'product_uom_id': self.product_uom_id.id,
             'name_field': self.name_field,
 
         })
