35bcbd8393473948d1cde99c3569d8ffd1ddddcd
najwa-56 <najwa-56@hotmail.com>
2024-08-25 14:30:17 +0300

    up

--- custom-addons/sh_secondary_unit/models/product_product.py
+++ custom-addons/sh_secondary_unit/models/product_product.py
@@ -90,7 +90,7 @@ class ShProductTemplate(models.Model):
     uom_id_1 = fields.Many2one('product.multi.uom.price', string='UOM 1', compute='_compute_uom_columns')
     uom_id_2 = fields.Many2one('product.multi.uom.price', string='UOM 2', compute='_compute_uom_columns')
     uom_id_3 = fields.Many2one('product.multi.uom.price', string='UOM 3', compute='_compute_uom_columns')
-
+    uom_id_4 = fields.Many2one('product.multi.uom.price', string='UOM 4', compute='_compute_uom_columns')
     @api.depends('selected_uom_ids')
     def _compute_uom_columns(self):
         for record in self:
@@ -98,6 +98,7 @@ class ShProductTemplate(models.Model):
             record.uom_id_1 = uoms[0] if len(uoms) > 0 else False
             record.uom_id_2 = uoms[1] if len(uoms) > 1 else False
             record.uom_id_3 = uoms[2] if len(uoms) > 2 else False
+            record.uom_id_4 = uoms[3] if len(uoms) > 3 else False
 
     '''show uoms in column'''
 
@@ -105,6 +106,7 @@ class ShProductTemplate(models.Model):
     uom_id_1_onhand1 = fields.Float('On Hand',compute='_compute_secondary_unit_on_hand_qty1')
     uom_id_2_onhand2 = fields.Float('On Hand', compute='_compute_secondary_unit_on_hand_qty2')
     uom_id_3_onhand3 = fields.Float('On Hand', compute='_compute_secondary_unit_on_hand_qty3')
+    uom_id_4_onhand4 = fields.Float('On Hand', compute='_compute_secondary_unit_on_hand_qty4')
     def _compute_secondary_unit_on_hand_qty1(self):
         for rec in self:
             if rec.uom_id and rec.uom_id_1 and rec.uom_id_1.uom_id:
@@ -130,6 +132,15 @@ class ShProductTemplate(models.Model):
                     rec.qty_available, rec.uom_id_3.uom_id)
             else:
                 rec.uom_id_3_onhand3 = 0.0
+
+    def _compute_secondary_unit_on_hand_qty4(self):
+        for rec in self:
+            if rec.uom_id and rec.uom_id_4 and rec.uom_id_4.uom_id:
+                # Assuming uom_id_1 has a field 'uom_id' that is of type 'uom.uom'
+                rec.uom_id_4_onhand4 = rec.uom_id._compute_quantity(
+                    rec.qty_available, rec.uom_id_4.uom_id)
+            else:
+                rec.uom_id_4_onhand4 = 0.0
 
     '''UOMs'''
 class ShStockQuant(models.Model):
