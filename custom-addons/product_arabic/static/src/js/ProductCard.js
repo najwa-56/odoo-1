ca8c458943d3acd79d78bdacf662e8857b3f803a
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-08-31 06:25:35 +0300

    Update ProductCard.js

--- custom-addons/product_arabic/static/src/js/ProductCard.js
+++ custom-addons/product_arabic/static/src/js/ProductCard.js
@@ -1,27 +1,26 @@
-/** @odoo-module */
-
 import { Component } from "@odoo/owl";
 import { ProductCard } from "@point_of_sale/app/generic_components/product_card/product_card";
 import { patch } from "@web/core/utils/patch";
 
-//Patched the ProductCard for defining that clickMagnifyProduct is function
-patch(ProductCard.prototype, {
-    //Supering setup() function
+// Patch the ProductCard to define new props or modify existing ones
+patch(ProductCard.prototype, "product-card-patch", {
     setup() {
+        // Always call the super setup if you're extending the setup method
         super.setup();
     },
 });
-ProductCard.props = {
-        class: { String, optional: true },
-        name: String,
-        productArabic: String,
-        productId: Number,
-        price: String,
-        imageUrl: String,
-        productInfo: { Boolean, optional: true },
-        onClick: { type: Function, optional: true },
-        onProductInfoClick: { type: Function, optional: true },
-};
 
+// Define or redefine props for the ProductCard component
+ProductCard.props = {
+    class: { type: String, optional: true },
+    name: { type: String },
+    productArabic: { type: String }, // Ensuring type is explicitly defined
+    productId: { type: Number },
+    price: { type: String },
+    imageUrl: { type: String },
+    productInfo: { type: Boolean, optional: true },
+    onClick: { type: Function, optional: true },
+    onProductInfoClick: { type: Function, optional: true },
+};
 
 
