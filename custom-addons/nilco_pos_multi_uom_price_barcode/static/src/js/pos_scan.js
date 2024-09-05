abce961a1496ee5a95e94a7b2b70499c603be1f6
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-09-05 09:52:54 +0300

    Update pos_scan.js

--- custom-addons/nilco_pos_multi_uom_price_barcode/static/src/js/pos_scan.js
+++ custom-addons/nilco_pos_multi_uom_price_barcode/static/src/js/pos_scan.js
@@ -29,11 +29,26 @@ patch(ProductScreen.prototype, {
                 },
             });
         } else if (code.type === "weight" || code.type === "quantity") {
-            Object.assign(options, {
-                quantity: code.value,
-                merge: false,
-            });
-        } else if (code.type === "discount") {
+    // If dealing with weight, calculate the price based on the weight.
+    if (code.type === "weight") {
+        const uom = this.env.pos.db.getUOMByBarcode(barcode); // Fetch the UOM linked to this barcode.
+        const unitPrice = uom.price; // Price per UOM unit.
+        const weight = code.value; // Weight value scanned from the barcode.
+        const calculatedPrice = unitPrice * weight; // Calculate total price based on weight.
+
+        Object.assign(options, {
+            price: calculatedPrice,
+            quantity: weight, // Treat weight as quantity in this context.
+            merge: false,
+        });
+    } else {
+        // If it's quantity without weight, just use the value as is.
+        Object.assign(options, {
+            quantity: code.value,
+            merge: false,
+        });
+    }
+} else if (code.type === "discount") {
             Object.assign(options, {
                 discount: code.value,
                 merge: false,
@@ -64,43 +79,57 @@ patch(DB.PosDB.prototype, {
         this._super.apply(this, arguments);
     },
     get_product_by_barcode(barcode) {
+    const barcodes = Object.values(this.product_multi_barcodes);
+    if (this.product_by_barcode[barcode]) {
+        return this.product_by_barcode[barcode];
+    } else if (this.product_packaging_by_barcode[barcode]) {
+        return this.product_by_id[this.product_packaging_by_barcode[barcode].product_id[0]];
+    } else if (barcodes.length > 0) {
+        for (const product of barcodes) {
+            const uoms = Object.values(product.uom_id);
+            for (const uom of uoms) {
+                if (uom.barcodes.includes(barcode)) {
+                    const result = this.product_by_id[uom.product_variant_id[0]];
+                    const line = new Orderline(
+                        { env: result.env },
+                        { pos: result.pos, order: result.pos.selectedOrder, product: result }
+                    );
 
-        const barcodes = Object.values(this.product_multi_barcodes);
-        if (this.product_by_barcode[barcode]) {
-            return this.product_by_barcode[barcode];
-        } else if (this.product_packaging_by_barcode[barcode]) {
-            return this.product_by_id[this.product_packaging_by_barcode[barcode].product_id[0]];
-        } else if (barcodes.length > 0) {
-            for (const product of barcodes) {
-                const uoms = Object.values(product.uom_id);
-                for (const uom of uoms) {
-                    if (uom.barcodes.includes(barcode)) {
-                        const result = this.product_by_id[uom.product_variant_id[0]];
-                        const line = new Orderline(
-                            { env: result.env },
-                            { pos: result.pos, order: result.pos.selectedOrder, product: result }
-                        );
-                        const orderlines = result.pos.selectedOrder.get_orderlines();
-                        for (const orderline of orderlines) {
-                            if (orderline.product.id === result.id &&
-                                orderline.product_uom_id[0] === uom.id &&
-                                orderline.price === uom.price) {
-                                orderline.set_quantity(orderline.quantity + 1, uom.price);
-                                  orderline.set_uom_name(orderline.name_field );
-                                return true;
-                            }
-                        }
-                        result.pos.selectedOrder.add_orderline(line);
-                        result.pos.selectedOrder.selected_orderline.set_uom({ 0: uom.id, 1: uom.name });
-                        result.pos.selectedOrder.selected_orderline.price_manually_set = true;
-                        result.pos.selectedOrder.selected_orderline.set_unit_price(uom.price);
-                         result.pos.selectedOrder.selected_orderline.set_uom_name(uom.name_field);
-                        return true;
+                    // Check if the scanned barcode type is for weight
+                    let calculatedPrice;
+                    if (barcode.type === 'weight') {
+                        const weight = barcode.value; // Weight value from the barcode.
+                        calculatedPrice = uom.price * weight; // Calculate price based on weight.
+                    } else {
+                        calculatedPrice = uom.price; // Use standard UOM price if not weight-based.
                     }
+
+                    // Update or create the order line with the calculated price
+                    const orderlines = result.pos.selectedOrder.get_orderlines();
+                    for (const orderline of orderlines) {
+                        if (orderline.product.id === result.id &&
+                            orderline.product_uom_id[0] === uom.id &&
+                            orderline.price === calculatedPrice) {
+                            orderline.set_quantity(orderline.quantity + 1, calculatedPrice);
+                            orderline.set_uom_name(uom.name_field);
+                            return true;
+                        }
+                    }
+
+                    // If no matching order line found, create a new one
+                    result.pos.selectedOrder.add_orderline(line);
+                    result.pos.selectedOrder.selected_orderline.set_uom({ 0: uom.id, 1: uom.name });
+                    result.pos.selectedOrder.selected_orderline.price_manually_set = true;
+                    result.pos.selectedOrder.selected_orderline.set_unit_price(calculatedPrice);
+                    result.pos.selectedOrder.selected_orderline.set_uom_name(uom.name_field);
+                    return true;
                 }
             }
-            return undefined;
         }
+    }
+    return undefined;
+}
+
         return undefined;
     },
 });
