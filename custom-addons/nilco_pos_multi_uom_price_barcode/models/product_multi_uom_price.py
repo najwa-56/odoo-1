b0b1ec7febf2a255409f067133e339a389f64948
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-09-08 10:13:28 +0300

    Update product_multi_uom_price.py

--- custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
+++ custom-addons/nilco_pos_multi_uom_price_barcode/models/product_multi_uom_price.py
@@ -5,7 +5,7 @@ from odoo.osv import expression
 class Inheritmulti_uom(models.Model):
     _inherit = 'product.multi.uom.price'
 
-    barcode = fields.Char('Barcode')
+    barcode = fields.Char('الباركود')
     product_variant_id =fields.Many2one('product.product',related="product_id.product_variant_id",store=True)
     product_variant_count = fields.Integer('Product Variant Count')
 
