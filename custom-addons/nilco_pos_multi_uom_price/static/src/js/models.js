680b64f77944ed1ae043372f05dd572f4dbedd7b
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-09-16 10:56:40 +0300

    Update models.js

--- custom-addons/nilco_pos_multi_uom_price/static/src/js/models.js
+++ custom-addons/nilco_pos_multi_uom_price/static/src/js/models.js
@@ -103,8 +103,7 @@ patch(Orderline.prototype, {
             // Move existing orderline to the end of the orderlines array
             this.order.orderlines = this.order.orderlines.filter(line => line !== existingOrderline);
             this.order.orderlines.push(existingOrderline);
-             // Set the pointer to the last orderline (i.e., the reordered one)
-        this.order.set_selected_orderline(existingOrderline);
+
         }
     },
     getDisplayData() {
