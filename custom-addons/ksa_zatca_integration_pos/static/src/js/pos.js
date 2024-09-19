ab47f9c39633b5708e92f180c87c5e870e90fde0
najwa-56 <najwa-56@hotmail.com>
2024-09-19 14:43:43 +0300

    Update pos.js

--- custom-addons/ksa_zatca_integration_pos/static/src/js/pos.js
+++ custom-addons/ksa_zatca_integration_pos/static/src/js/pos.js
@@ -2,15 +2,24 @@ import { PartnerDetailsEdit } from "@point_of_sale/app/screens/partner_list/part
 import { patch } from "@web/core/utils/patch";
 import { _t } from "@web/core/l10n/translation";
 
+console.log('PartnerDetailsEdit before patch:', PartnerDetailsEdit);
+
 patch(PartnerDetailsEdit.prototype, 'custom-partner-details-edit', {
     setup() {
-        this._super.apply(this, arguments);  // Correctly call super in patched setup method
+        console.log('PartnerDetailsEdit patch setup called');
+        this._super.apply(this, arguments);  // Correctly call super
         const partner = this.props.partner;
+
+        // Check if partner data is available
+        console.log('Partner data:', partner);
 
         // Add new fields to changes state
         this.changes.building_no = partner.building_no || "";
         this.changes.additional_no = partner.additional_no || "";
         this.changes.district = partner.district || "";
+
+        // Log changes for debugging
+        console.log('Changes state after adding new fields:', this.changes);
 
         // Optionally, add translated terms for the new fields
         this.partnerDetailsFields = {
@@ -19,5 +28,9 @@ patch(PartnerDetailsEdit.prototype, 'custom-partner-details-edit', {
             'Additional No': _t('Additional No'),
             'District': _t('District'),
         };
+
+        console.log('Updated partnerDetailsFields:', this.partnerDetailsFields);
     },
 });
+
+console.log('PartnerDetailsEdit after patch:', PartnerDetailsEdit);
