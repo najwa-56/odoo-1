f11bfad6aa7d5769cddc780b9c4c604804969006
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-09-19 12:00:56 +0300

    Update pos_scan.js

--- custom-addons/nilco_pos_multi_uom_price_barcode/static/src/js/pos_scan.js
+++ custom-addons/nilco_pos_multi_uom_price_barcode/static/src/js/pos_scan.js
@@ -7,9 +7,6 @@ import { Order, Orderline, Payment } from "@point_of_sale/app/store/models";
 import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product_screen";
 const core = require('web.core');
 const _t = core._t;
-import { ProductsWidget } from "@point_of_sale/app/screens/product_screen/product_list/product_list";
-import { useService } from "@web/core/utils/hooks";
-import { useState } from "owl";
 import { ErrorBarcodePopup } from "@point_of_sale/app/barcode/error_popup/barcode_error_popup";
 let lastBarcodeTime = 0;
 const debounceTime = 100;  // Adjust delay as needed
@@ -23,14 +20,7 @@ function handleBarcode(barcode, callback) {
     callback();  // Call the original barcode processing logic
 
 }
-patch(ProductsWidget.prototype, {
-  setup() {
-        super.setup();
-        this.pos=usePos();
-    },
 
-
-});
 patch(ProductScreen.prototype, {
     async _barcodeProductAction(code) {
 
