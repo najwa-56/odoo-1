c8dd3570bcb89d7cb43de38082c43595c46d6870
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-09-19 07:14:49 +0300

    Update pos_scan.js

--- custom-addons/nilco_pos_multi_uom_price_barcode/static/src/js/pos_scan.js
+++ custom-addons/nilco_pos_multi_uom_price_barcode/static/src/js/pos_scan.js
@@ -75,11 +75,15 @@ patch(DB.PosDB.prototype, {
         this._super.apply(this, arguments);
         this.initialQuantities = {}; // Initialize initial quantities object
     },
-    // Modify the search string to include UOM barcodes
-    _product_search_string: function(product) {
-        let str = this._super(product);
-        const uoms = Object.values(product.uom_id);
+     // Modify the search string to include UOM barcodes
+    _product_search_string: function (product) {
+        let str = product.display_name;
+        if (product.barcode) {
+            str += '|' + product.barcode;
+        }
 
+        // Check UOM barcodes and add them to the search string
+        const uoms = Object.values(product.uom_id);
         for (const uom of uoms) {
             if (uom.barcodes && uom.barcodes.length) {
                 for (const barcode of uom.barcodes) {
@@ -87,6 +91,8 @@ patch(DB.PosDB.prototype, {
                 }
             }
         }
+
+        str = product.id + ':' + str;
         return str;
     },
     get_product_by_barcode(barcode) {
