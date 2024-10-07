2f05b87a6e025dc0eb2eb669fccd19c76a4086f9
sysadminRJ7 <sysadmin@rj7.com.sa>
2024-10-07 12:00:14 +0300

    add map button to route manangment moduel

--- addons/customer_route_management_editing/__manifest__.py
+++ addons/customer_route_management_editing/__manifest__.py
@@ -31,7 +31,7 @@
     'website': 'https://www.cybrosys.com',
     'live_test_url': 'https://www.youtube.com/watch?v=tdu6iBP2N6Y',
     'category': 'Tools',
-    'depends': ['base', 'mail','sale','customer_route_management'],
+    'depends': ['base', 'mail','sale','customer_route_management','google_maps_partner'],
     'license': 'OPL-1',
     'price': 19.99,
     'currency': 'EUR',
