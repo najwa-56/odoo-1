50c8fbe2aa1c8d7868f7cad31507bc6da3f48ef9
sysadminRJ7 <sysadmin@rj7.com.sa>
2024-10-26 00:05:54 +0300

    pos scan using uom_id as primary key

--- custom-addons/nilco_pos_multi_uom_price_barcode/static/src/js/pos_scan.js
+++ custom-addons/nilco_pos_multi_uom_price_barcode/static/src/js/pos_scan.js
@@ -62,28 +62,53 @@ patch(ProductScreen.prototype, {
         
 
     // Check if the product exists in pos_multi_op
-        let product_tmpl_id = product.product_tmpl_id;
-        if (pos_multi_op[product_tmpl_id]) {
-            let barcodePrices = pos_multi_op[product_tmpl_id].barcodes;
+        // let product_tmpl_id = product.product_tmpl_id;
+        // if (pos_multi_op[product_tmpl_id]) {
+        //     let barcodePrices = pos_multi_op[product_tmpl_id].barcodes;
 
-            // Loop through the barcode data for the product
-            Object.keys(barcodePrices).forEach(function(barcode) {
-                if (barcode === code.base_code) {
-                    let uom_data = barcodePrices[barcode];
+        //     // Loop through the barcode data for the product
+        //     Object.keys(barcodePrices).forEach(function(barcode) {
+        //         if (barcode === code.base_code) {
+        //             let uom_data = barcodePrices[barcode];
+        //             unit_price = uom_data.price;
+        //             uom_data_matched = true;
+        //             selected_uom_id = uom_data.uom_id[0];
+        //             selected_uom_name = uom_data.name_field;
+
+        //             Object.assign(options, {
+        //                 price: uom_data.price,
+        //                 extras: {
+        //                     wvproduct_uom: this.pos.units_by_id[uom_data.uom_id[0]],
+        //                 },
+        //             });
+        //         }
+        //     }, this);
+        // }
+
+        let product_tmpl_id = product.product_tmpl_id;
+
+        // Check if the product exists in pos_multi_op
+        if (pos_multi_op[product_tmpl_id]) {
+            let uomPrices = pos_multi_op[product_tmpl_id].uom_id;
+
+            // Loop through the UOM data for the product
+            Object.values(uomPrices).forEach(function(uom_data) {
+                if (uom_data.barcodes.includes(code.base_code)) {
                     unit_price = uom_data.price;
                     uom_data_matched = true;
-                    selected_uom_id = uom_data.uom_id[0];
+                    selected_uom_id = uom_data.id;
                     selected_uom_name = uom_data.name_field;
 
                     Object.assign(options, {
                         price: uom_data.price,
                         extras: {
-                            wvproduct_uom: this.pos.units_by_id[uom_data.uom_id[0]],
+                            wvproduct_uom: this.pos.units_by_id[uom_data.id],
                         },
                     });
                 }
             }, this);
         }
+
 
         // If UOM barcode wasn't matched, fallback to original product barcode
         if (!uom_data_matched) {
