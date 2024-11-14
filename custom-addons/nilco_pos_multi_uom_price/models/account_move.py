a09400ea62c8a50a9964d12d8be988169603abd6
najwa-56 <najwa-56@hotmail.com>
2024-11-14 14:17:51 +0300

    Update account_move.py

--- custom-addons/nilco_pos_multi_uom_price/models/account_move.py
+++ custom-addons/nilco_pos_multi_uom_price/models/account_move.py
@@ -16,4 +16,17 @@ class AccountMoveLine(models.Model):
                 rec.uom_name = uom_id[0].name_field
             else:
                 rec.uom_name = rec.product_uom_id.name
-           
+
+class AccountINvoiceREport(models.Model):
+    _inherit = "account.invoice.report"
+
+    uom_name = fields.Char('Uom Name', compute="_compute_get_uom_name")
+
+    @api.depends('product_uom_id')
+    def _compute_get_uom_name(self):
+        for rec in self:
+            uom_id = rec.product_id.multi_uom_price_id.filtered(lambda m :m.uom_id.id == rec.product_uom_id.id)
+            if uom_id:
+                rec.uom_name = uom_id[0].name_field
+            else:
+                rec.uom_name = rec.product_uom_id.name
