83e08b5e63847585625a9beb2bc30b5b630e826e
najwa-56 <najwa-56@hotmail.com>
2024-11-17 13:58:24 +0300

    update

--- custom-addons/nilco_pos_multi_uom_price/models/account_move.py
+++ custom-addons/nilco_pos_multi_uom_price/models/account_move.py
@@ -26,8 +26,8 @@ class AccountInvoiceReport(models.Model):
     @api.depends('product_uom_id')
     def _compute_get_uom_name(self):
         for rec in self:
-            uom_id = rec.product_id.multi_uom_price_id.filtered(lambda m: m.uom_id.id == rec.product_uom_id.id)
+            uom_id = rec.product_id.multi_uom_price_id.filtered(lambda m :m.uom_id.id == rec.product_uom_id.id)
             if uom_id:
-                rec.uom_name = uom_id[0].name_field or ''
+                rec.uom_name = uom_id[0].name_field
             else:
-                rec.uom_name = rec.product_uom_id.name or ''
+                rec.uom_name = rec.product_uom_id.name
