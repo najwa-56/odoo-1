a8ddc121f737d854833ede8c5c278309aeeb636d
najwa-56 <najwa-56@hotmail.com>
2024-11-17 14:15:28 +0300

    Update account_move.py

--- custom-addons/nilco_pos_multi_uom_price/models/account_move.py
+++ custom-addons/nilco_pos_multi_uom_price/models/account_move.py
@@ -21,13 +21,13 @@ class AccountMoveLine(models.Model):
 class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
 
-    uom_name = fields.Char('UoM Name', compute="_compute_get_uom_name", store=True)
 
-    @api.depends('product_uom_id')
-    def _compute_get_uom_name(self):
+    name_field = fields.Char(string="أسم الوحدة", compute="_compute_name_field", store=True)
+    @api.depends('product_uom')
+    def _compute_name_field(self):
         for rec in self:
-            uom_id = rec.product_id.multi_uom_price_id.filtered(lambda m :m.uom_id.id == rec.product_uom_id.id)
+            uom_id = rec.product_id.multi_uom_price_id.filtered(lambda m :m.uom_id.id == rec.product_uom.id)
             if uom_id:
-                rec.uom_name = uom_id[0].name_field
+                rec.name_field = uom_id[0].name_field
             else:
-                rec.uom_name = rec.product_uom_id.name
+                rec.name_field = rec.product_uom.name
