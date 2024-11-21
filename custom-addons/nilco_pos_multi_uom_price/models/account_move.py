e0d84e34bd061bde4e816c9bf81c3de199f9c057
najwa-56 <najwa-56@hotmail.com>
2024-11-21 13:28:27 +0300

    Update account_move.py

--- custom-addons/nilco_pos_multi_uom_price/models/account_move.py
+++ custom-addons/nilco_pos_multi_uom_price/models/account_move.py
@@ -18,22 +18,12 @@ class AccountMoveLine(models.Model):
                 rec.uom_name = rec.product_uom_id.name
 
 
+
 class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
 
+    # Adding new fields
     uom_name = fields.Char(string="UOM Name", store=True)
-
-    def _select(self):
-        select_str = super(AccountInvoiceReport, self)._select()
-        select_str += ", line.uom_name as uom_name"
-        return select_str
-
-    def _group_by(self):
-        group_by_str = super(AccountInvoiceReport, self)._group_by()
-        group_by_str += ", line.uom_name"
-        return group_by_str
-
-
     qty = fields.Float(string='Adjusted Quantity', compute='_compute_qty', store=True)
 
     @api.depends('quantity', 'product_id')
@@ -52,20 +42,26 @@ class AccountInvoiceReport(models.Model):
     def _select(self):
         select_str = super(AccountInvoiceReport, self)._select()
         select_str += """
-                    , COALESCE(line.quantity / NULLIF(multi_uom_price.ratio, 0), 0) as qty
-                """
+            , line.uom_name as uom_name
+            , COALESCE(line.quantity / NULLIF(multi_uom_price.ratio, 0), 0) as qty
+        """
         return select_str
 
     def _from(self):
         from_str = super(AccountInvoiceReport, self)._from()
         from_str += """
-                    LEFT JOIN product_multi_uom_price AS multi_uom_price
-                    ON multi_uom_price.product_id = line.product_id
-                    AND multi_uom_price.uom_id = line.product_uom_id
-                """
+            LEFT JOIN product_multi_uom_price AS multi_uom_price
+            ON multi_uom_price.product_id = line.product_id
+            AND multi_uom_price.uom_id = line.product_uom_id
+        """
         return from_str
 
     def _group_by(self):
         group_by_str = super(AccountInvoiceReport, self)._group_by()
-        group_by_str += ", line.quantity, multi_uom_price.ratio"
+        group_by_str += """
+            , line.uom_name
+            , line.quantity
+            , multi_uom_price.ratio
+        """
         return group_by_str
+
