19128c3fbb40118416caea9a5cb0effa1e40d8da
najwa-56 <najwa-56@hotmail.com>
2024-11-24 11:40:52 +0300

    Update account_move.py

--- custom-addons/nilco_pos_multi_uom_price/models/account_move.py
+++ custom-addons/nilco_pos_multi_uom_price/models/account_move.py
@@ -168,18 +168,19 @@ class AccountInvoiceReport(models.Model):
         return select_str
 
     def _from(self):
-        """Extend the SQL FROM statement to include UOM details."""
+        """Extend the SQL FROM statement to join with UOM details if not already included."""
         from_str = super(AccountInvoiceReport, self)._from()
 
-        # Avoid duplicate joins
-        if "LEFT JOIN uom_uom uom_line" not in from_str:
+        # Avoid duplicate joins by checking for `uom_line` and `uom_template`
+        if "uom_line" not in from_str:
             from_str += """
                 LEFT JOIN uom_uom uom_line ON uom_line.id = line.product_uom_id
             """
-        if "LEFT JOIN uom_uom uom_template" not in from_str:
+        if "uom_template" not in from_str:
             from_str += """
                 LEFT JOIN uom_uom uom_template ON uom_template.id = template.uom_id
             """
+
         return from_str
 
     def _group_by(self):
