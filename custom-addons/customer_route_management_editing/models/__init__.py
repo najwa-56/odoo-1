133761d6d8bd38292b8fcba672318561fab331ee
najwa-56 <najwa-56@hotmail.com>
2024-11-27 14:04:08 +0300

    removing

--- custom-addons/customer_route_management_editing/models/__init__.py
+++ custom-addons/customer_route_management_editing/models/__init__.py
@@ -2,4 +2,3 @@ from . import delivery_route
 from . import res_partner
 from . import route_line
 from . import sale_order
-from . import account_move
--- custom-addons/customer_route_management_editing/models/account.move.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class AccountMove(models.Model):
-    _inherit = 'account.move'
-
-    location_id = fields.Many2one('route.line', string='Location', help="Location of route.", related='partner_id.location_id', store=True)
