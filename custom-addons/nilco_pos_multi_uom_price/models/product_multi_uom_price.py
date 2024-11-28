0a16fdbe9822bcecfc0f12a923a252be2af7be9d
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-11-28 11:07:11 +0300

    applay_update_cost_value_in product_templet

--- custom-addons/nilco_pos_multi_uom_price/models/product_multi_uom_price.py
+++ custom-addons/nilco_pos_multi_uom_price/models/product_multi_uom_price.py
@@ -9,12 +9,22 @@ class multi_uom(models.Model):
     category_id = fields.Many2one(related='product_id.uom_id.category_id', index=True)
     uom_id = fields.Many2one('uom.uom', string="الوحدة", domain="[('category_id', '=', category_id)]", index=True)#,required=True
     price = fields.Float(string='السعر',required=True,digits='Product Price')
-    cost = fields.Float(string='التكلفة',required=True,digits='Product Cost')
+    cost = fields.Float(string='التكلفة', required=True ,digits='Product Cost',compute="_compute_calculated_cost")
     qty = fields.Float(string="الكمية" )
     name_field = fields.Char(store=True, string="أسم الوحدة")
     ratio = fields.Float(related='uom_id.ratio', string="Ratio", store=True)
 
-   # _sql_constraints = [
+    @api.depends('ratio', 'product_id.standard_price')
+    def _compute_calculated_cost(self):
+        for record in self:
+            if record.ratio and record.product_id.standard_price:
+                record.cost = record.ratio * record.product_id.standard_price
+            else:
+                record.cost = 0.0
+
+# _sql_constraints = [
      #   ('product_multi_uom_price_uniq',
       #   'UNIQUE (product_id,uom_id)',
         # _('UOM Product Must Be Unique !'))]
+
+
