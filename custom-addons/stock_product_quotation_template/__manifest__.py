5816a9e4b93006d862d86879b3c00bfc36c5ca89
sysadminRJ7 <sysadmin@rj7.com.sa>
2024-12-01 13:19:10 +0300

    quotiaon template for stock

---
+++ custom-addons/stock_product_quotation_template/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Stock Product Quotation Template',
+    'version': '1.0',
+    'category': 'Stock',
+    'sequence': 6,
+    'author': 'Adnan Oscar',
+    'summary': 'Stock Product Quotation Template',
+    'description': """
+       Stock Product Quotation Template
+    """,
+    'depends': ['sale','stock'],
+    'data': [
+        'views/views.xml',
+    ],
+    
+   
+    'installable': True,
+    'website': '',
+    'auto_install': False,
+    
+}
---
+++ custom-addons/stock_product_quotation_template/models/picking.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from datetime import timedelta
+from functools import partial
+import psycopg2
+from odoo import api, fields, models, tools, _
+from odoo.tools import float_is_zero
+from odoo.exceptions import UserError
+from odoo.http import request
+import odoo.addons.decimal_precision as dp
+from itertools import groupby
+import json
+
+class StockPicking(models.Model):
+    _inherit='stock.picking'
+
+    sale_order_template_id = fields.Many2one(
+        comodel_name='sale.order.template',
+        string="Quotation Template",
+        compute='_compute_sale_order_template_id',
+        store=True, readonly=False, check_company=True, precompute=True,
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
+    
+
+    def _compute_sale_order_template_id(self):
+        for order in self:
+            company_template = order.company_id.sale_order_template_id
+            if company_template and order.sale_order_template_id != company_template:
+                if 'website_id' in self._fields and order.website_id:
+                    # don't apply quotation template for order created via eCommerce
+                    continue
+                order.sale_order_template_id = order.company_id.sale_order_template_id.id
+
+
+    @api.onchange('sale_order_template_id')
+    def _onchange_sale_order_template_id(self):
+        if not self.sale_order_template_id:
+            return
+
+        sale_order_template = self.sale_order_template_id.with_context(lang=self.partner_id.lang)
+
+        order_lines_data = [fields.Command.clear()]
+        order_lines_data += [
+            fields.Command.create(line._prepare_order_line_values())
+            for line in sale_order_template.sale_order_template_line_ids
+        ]
+
+        # set first line to sequence -99, so a resequence on first page doesn't cause following page
+        # lines (that all have sequence 10 by default) to get mixed in the first page
+        if len(order_lines_data) >= 2:
+            order_lines_data[1][2]['sequence'] = -99
+
+        self.move_ids_without_package = order_lines_data
+
+     
+
+class SaleOrderTemplateLine(models.Model):
+    _inherit = 'sale.order.template.line'
+
+    def _prepare_order_line_values(self):
+        res = super()._prepare_order_line_values()
+        # Check if 'params' exists in context and contains 'model' equal to 'stock.picking'
+        if self.env.context.get('params', False) and self.env.context['params'].get('model', False) == 'stock.picking':
+            res.pop('display_type', None)  # Safely remove 'display_type' if it exists
+
+        return res
