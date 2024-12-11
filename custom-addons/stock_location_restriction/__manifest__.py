09f50bc663c3b934973a66ac60a372bb09f1e6f5
sysadminRJ7 <sysadmin@rj7.com.sa>
2024-12-11 10:54:49 +0300

    restirc location for users

---
+++ custom-addons/stock_location_restriction/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Restriction on Stock Location",
+
+    'version': "17.0",
+
+    'category': "Stock",
+
+    
+    
+
+    "depends": ['stock'],
+    
+    "data": [
+        "security/security.xml",
+        "views/stock_location_view.xml",
+    ],
+
+    'license': "OPL-1",
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
