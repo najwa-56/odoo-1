1429d5e88f9642bb9a3ccde81f2a4ae55b56c4db
sysadminRJ7 <sysadmin@rj7.com.sa>
2025-01-06 13:25:34 +0300

    sale order for dolfin customer without taxes

--- custom-addons/sale_order_automation/models/sale_order.py
+++ custom-addons/sale_order_automation/models/sale_order.py
@@ -10,16 +10,16 @@ class SaleOrder(models.Model):
         if self.invoice_ids:
             return self.env.ref('ksa_zatca_integration.action_report_standard_low_margin_tax_invoice').report_action(self.invoice_ids.ids)
 
-    @api.onchange('partner_id', 'sale_order_template_id')
-    def onchange_partner_id_dolfin(self):
-        for order in self:
-            if order.partner_id and order.partner_id.is_dolfin:
-                for line in order.order_line:
-                    # Remove taxes
-                    line.tax_id = False
-                    # Adjust price_unit by subtracting 15%
-                    if line.price_unit:
-                        line.price_unit = line.price_unit / 1.15
+    # @api.onchange('partner_id', 'sale_order_template_id')
+    # def onchange_partner_id_dolfin(self):
+    #     for order in self:
+    #         if order.partner_id and order.partner_id.is_dolfin:
+    #             for line in order.order_line:
+    #                 # Remove taxes
+    #                 line.tax_id = False
+    #                 # Adjust price_unit by subtracting 15%
+    #                 if line.price_unit:
+    #                     line.price_unit = line.price_unit / 1.15
 
 
 
@@ -121,3 +121,19 @@ class SaleOrder(models.Model):
                     payment_register.action_create_payments()
 
         return res  
+
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+    
+   
+    @api.onchange('product_uom_qty')
+    def onchange_partner_id_dolfin(self):
+        for line in self:
+            if line.order_id.partner_id and line.order_id.partner_id.is_dolfin:
+                # Remove taxes
+                line.tax_id = False
+                # Adjust price_unit by subtracting 15%
+                if line.price_unit:
+                    line.price_unit = line.price_unit / 1.15
