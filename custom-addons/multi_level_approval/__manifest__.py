845531092a98453ce5388c5aa75afb787d8c50e4
sysadminRJ7 <sysadmin@rj7.com.sa>
2025-02-07 11:03:23 +0300

    add moduel for approval

---
+++ custom-addons/multi_level_approval/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Approval',
+    'version': '17.0.1.0',
+    'category': 'Approvals',
+    'description': """
+Odoo Approval Module: Multi level approval - create and validate approvals requests.
+Each request can be approve by many levels of different managers.
+The managers wil review and approve sequentially
+    """,
+    'summary': '''
+    Create and validate approval requests. Each request can be approved by many levels of different managers
+    ''',
+    'depends': [
+        'mail',
+        'product'
+    ],
+    'data': [
+        'data/ir_sequence_data.xml',
+        'data/mail_template_data.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv',
+
+        # wizard
+        'wizard/refused_reason_views.xml',
+
+        'views/multi_approval_type_views.xml',
+        'views/multi_approval_views.xml',
+
+        # Add actions after all views.
+        'views/actions.xml',
+
+        # Add menu after actions.
+        'views/menu.xml',
+        
+    ],
+    
+}
