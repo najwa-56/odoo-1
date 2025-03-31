05065c79d76ee386a81feab8a4de39a2adf9dc1a
sysadminRJ7 <sysadmin@rj7.com.sa>
2025-03-31 12:22:41 +0300

    invoice send

--- custom-addons/ksa_zatca_integration/models/account_move.py
+++ custom-addons/ksa_zatca_integration/models/account_move.py
@@ -1934,6 +1934,7 @@ class AccountMove(models.Model):
                         record.send_for_reporting()
                         self.env.cr.commit()
             except Exception as e:
+                self.env.cr.commit()
                 # Bypass errors.
                 _logger.info(f"Multi Send To Zatca Errors (Invoice ID: {record.id}) ***************************** :: {str(e)}")
 
