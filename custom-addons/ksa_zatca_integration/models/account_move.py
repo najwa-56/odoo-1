5f67b1a2baab1e843181e1a60241b25314030323
sysadminRJ7 <sysadmin@rj7.com.sa>
2025-03-31 12:45:00 +0300

    invoice send

--- custom-addons/ksa_zatca_integration/models/account_move.py
+++ custom-addons/ksa_zatca_integration/models/account_move.py
@@ -1878,7 +1878,7 @@ class AccountMove(models.Model):
 
 
     def send_invoice_batch(self, batch_size=600):
-        batch_size = 400
+        batch_size = 300
         start_date = datetime(2024, 8, 31).date() 
         end_date = datetime(2025, 3, 30).date() 
         invoices = self.sudo().search([
@@ -1893,7 +1893,6 @@ class AccountMove(models.Model):
         if not invoices:
             _logger.info(f"no left invoices lenght (Invoice ID: {len(invoices)}) *****************************")
             return
-        _logger.info(f"first invoices lenght (Invoice ID: {len(invoices)}) *****************************")
        
         invoices = invoices.filtered(lambda inv: all(line.tax_ids for line in inv.invoice_line_ids))
         _logger.info(f"fitlered invoice Send To Zatca Errors (Invoice ID: {len(invoices)}) *****************************")
@@ -1929,13 +1928,12 @@ class AccountMove(models.Model):
                             line.name = line.name.replace('&', 'و')
                     
                     if record.l10n_sa_invoice_type == 'Standard':
-                        _logger.info(f"l10n_sa_invoice_type standard Send To Zatca Errors (Invoice ID: {record.id}) ***************************** ")
-                        record.send_for_clearance()
-                        self.env.cr.commit()
+                        cleared = record.send_for_clearance()
+                        _logger.info(f"l10n_sa_invoice_type standard Send To Zatca (Invoice ID: {record.id}) - Response: {cleared}")
                     elif record.l10n_sa_invoice_type == 'Simplified':
-                        _logger.info(f"l10n_sa_invoice_type Simplified Send To Zatca Errors (Invoice ID: {record.id}) ***************************** ")
-                        record.send_for_reporting()
-                        self.env.cr.commit()
+                        report = record.send_for_reporting()
+                        _logger.info(f"l10n_sa_invoice_type standard Send To Zatca (Invoice ID: {record.id}) - Response: {cleared}")
+                        
             except Exception as e:
                 # Bypass errors.
                 _logger.info(f"Multi Send To Zatca Errors (Invoice ID: {record.id}) ***************************** :: {str(e)}")
