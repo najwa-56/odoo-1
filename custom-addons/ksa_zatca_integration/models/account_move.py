47db4c40a1fa6a20d2a6c09ae02d7fbc448dc583
sysadminRJ7 <sysadmin@rj7.com.sa>
2025-03-31 06:13:13 +0300

    invice sent

--- custom-addons/ksa_zatca_integration/models/account_move.py
+++ custom-addons/ksa_zatca_integration/models/account_move.py
@@ -1874,8 +1874,8 @@ class AccountMove(models.Model):
 
 
 
-    def send_invoice_batch(self, batch_size=200):
-        batch_size = 400
+    def send_invoice_batch(self, batch_size=600):
+        batch_size = 600
         start_date = datetime(2024, 8, 31).date() 
         end_date = datetime(2025, 12, 31).date() 
         invoices = self.sudo().search([
@@ -1889,6 +1889,9 @@ class AccountMove(models.Model):
             ('zatca_compliance_invoices_api', '=', False)
         ], limit=batch_size)
 
+        if not invoices:
+            _logger.info(f"no left invoices lenght (Invoice ID: {len(invoices)}) *****************************")
+            return
         _logger.info(f"first invoices lenght (Invoice ID: {len(invoices)}) *****************************")
        
         invoices = invoices.filtered(lambda inv: all(line.tax_ids for line in inv.invoice_line_ids))
