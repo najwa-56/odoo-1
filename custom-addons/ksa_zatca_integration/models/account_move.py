52c2dae6c1797c2f907f891202aaec7b79ca8ce7
sysadminRJ7 <sysadmin@rj7.com.sa>
2025-03-31 13:47:07 +0300

    invoice send

--- custom-addons/ksa_zatca_integration/models/account_move.py
+++ custom-addons/ksa_zatca_integration/models/account_move.py
@@ -1878,7 +1878,7 @@ class AccountMove(models.Model):
 
 
     def send_invoice_batch(self, batch_size=600):
-        batch_size = 200
+        batch_size = 100
         start_date = datetime(2024, 8, 31).date() 
         end_date = datetime(2025, 3, 30).date() 
         invoices = self.sudo().search([
