2eb87b04a3d4b44d219745df0c9b7357f29f5c36
sysadminRJ7 <sysadmin@rj7.com.sa>
2025-04-02 01:57:32 +0300

    order invoice

--- custom-addons/ksa_zatca_integration/models/account_move.py
+++ custom-addons/ksa_zatca_integration/models/account_move.py
@@ -1928,7 +1928,7 @@ class AccountMove(models.Model):
         
         for record in invoices:
             if not record.zatca_invoice_name or not record.zatca_compliance_invoices_api or record.zatca_status_code == '400':
-               if not (record.partner_id.vat.startswith('3') and record.partner_id.vat.endswith('3')):
+                if not (record.partner_id.vat.startswith('3') and record.partner_id.vat.endswith('3')):
                     record.partner_id.vat = '300000000000003'
            
                 if record.partner_id.is_company and len(record.partner_id.vat) != 15:
