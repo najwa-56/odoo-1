ef837edec525e87827eec3305f6af7ca0019b9ac
sysadminRJ7 <sysadmin@rj7.com.sa>
2025-04-21 10:58:29 +0300

    invoice sent

--- custom-addons/ksa_zatca_integration/models/account_move.py
+++ custom-addons/ksa_zatca_integration/models/account_move.py
@@ -1985,7 +1985,7 @@ class AccountMove(models.Model):
                     _logger.info(f"Multi Send To Zatca Errors (Invoice ID: {record.id}) ***************************** :: {str(e)}")
 
                     mail_values = {
-                        'subject': f"Sent Invoice To Zatca Processing Failed for {rec.name}",
+                        'subject': f"Sent Invoice To Zatca Processing Failed for {record.name}",
                         'body_html': f"<p><strong>Error:</strong> {str(e)}</p>",
                         'email_to': ','.join(recipients),
                         }
