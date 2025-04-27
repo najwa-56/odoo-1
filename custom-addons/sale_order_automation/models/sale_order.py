ffc21def35fe51ad54a7136b5fcb70c6b5d5f7fc
sysadminRJ7 <sysadmin@rj7.com.sa>
2025-04-27 08:17:06 +0300

    zatca integartion sale order automation

--- custom-addons/sale_order_automation/models/sale_order.py
+++ custom-addons/sale_order_automation/models/sale_order.py
@@ -8,7 +8,7 @@ class SaleOrder(models.Model):
 
     def action_print_standard_invoice(self):
         if self.invoice_ids:
-            return self.env.ref('ksa_zatca_integration.action_report_standard_low_margin_tax_invoice').report_action(self.invoice_ids.ids)
+            return self.env.ref('othaim_zatca_integration.action_report_standard_low_margin_tax_invoice').report_action(self.invoice_ids.ids)
 
     # @api.onchange('partner_id', 'sale_order_template_id')
     # def onchange_partner_id_dolfin(self):
