4f5bbe564b7b2e373815b9b6b638f758793d8369
sysadminRJ7 <sysadmin@rj7.com.sa>
2025-04-29 14:20:42 +0300

    zatca invoice

--- custom-addons/othaim_zatca_integration/models/account_move.py
+++ custom-addons/othaim_zatca_integration/models/account_move.py
@@ -1,14 +1,25 @@
 from odoo import api, fields, models, exceptions, _
+from dateutil.relativedelta import relativedelta
+from odoo.tools.float_utils import float_round
 
+import datetime
+import binascii
+import requests
 import logging
+import hashlib
+import base64
+import uuid
+import json
+import math
 
 from datetime import date
 from datetime import datetime
 from datetime import  timedelta
 
 
-
 _logger = logging.getLogger(__name__)
+
+
 
 
 class AccountMove(models.Model):
@@ -24,6 +35,28 @@ class AccountMove(models.Model):
         return self.currency_id.with_context(lang=lang).amount_to_text(amount)
     
 
+    
+
+    @api.model
+    def get_qr_code(self):
+
+        def get_qr_encoding(tag, field):
+            company_name_byte_array = field.encode('UTF-8')
+            company_name_tag_encoding = tag.to_bytes(length=1, byteorder='big')
+            company_name_length_encoding = len(company_name_byte_array).to_bytes(length=1, byteorder='big')
+            return company_name_tag_encoding + company_name_length_encoding + company_name_byte_array
+
+        qr_code_str = ''
+        seller_name_enc = get_qr_encoding(1, self.company_id.display_name)
+        company_vat_enc = get_qr_encoding(2, self.company_id.vat or '')
+        time_sa = fields.Datetime.context_timestamp(self.with_context(tz='Asia/Riyadh'), self.l10n_sa_confirmation_datetime or self.create_date)
+        timestamp_enc = get_qr_encoding(3, time_sa.isoformat())
+        invoice_total_enc = get_qr_encoding(4, str(self.amount_total))
+        total_vat_enc = get_qr_encoding(5, str(self.currency_id.round(self.amount_total - self.amount_untaxed)))
+
+        str_to_encode = seller_name_enc + company_vat_enc + timestamp_enc + invoice_total_enc + total_vat_enc
+        qr_code_str = base64.b64encode(str_to_encode).decode('UTF-8')
+        return qr_code_str
     
 
     def send_invoice_batch(self):
