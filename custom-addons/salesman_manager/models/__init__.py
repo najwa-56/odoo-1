44e91afb97685c5a72dd1b6f177e91f4f76fd6bc
sysadminRJ7 <sysadmin@rj7.com.sa>
2025-06-01 01:25:49 +0300

    sale manager module update

--- custom-addons/salesman_manager/models/__init__.py
+++ custom-addons/salesman_manager/models/__init__.py
@@ -7,7 +7,7 @@ from . import route_line
 from . import res_partner
 from . import sale_order_line
 from . import sale_order
-from . import daily_visit_report
 from . import res_partner_category
+from . import daily_visit_report
 from . import account_move
 from . import daily_realtime_report
