62ca65687e650acc6c281adc0309b09cc8334b8f
sysadminRJ7 <sysadmin@rj7.com.sa>
2025-07-30 13:42:20 +0300

    pos access rules

---
+++ custom-addons/pos_access_rules/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "POS ACCESS RULE ",
+    "category": "Point of Sale",
+    "license": "OPL-1",
+    "summary": "User See just own pos ",
+    "description": """ User See just own pos User See just own pos """,
+    "version": "0.0.1",
+    "depends": ['point_of_sale','web'],
+    "application": True,
+    "data": [
+        'views/res_users.xml',
+    ],
+    "auto_install": False,
+    "installable": True,
+
+}
