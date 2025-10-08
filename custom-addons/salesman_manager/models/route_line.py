a29fdee7190483752b95ed587a0f95b53dd70245
sysadminRJ7 <sysadmin@rj7.com.sa>
2025-10-08 14:03:27 +0300

    sale manager new version

--- custom-addons/salesman_manager/models/route_line.py
+++ custom-addons/salesman_manager/models/route_line.py
@@ -4,9 +4,9 @@ from odoo import models, fields, api
 class RouteLine(models.Model):
     _inherit = 'route.line'
 
-    #the domain  excludes records where both conditions are true is_later = False , create_date >= 2025-08-08 00:00:00:
-    #    cust_tree_ids = fields.One2many(domain=['|', ('create_date', '<', '2025-08-08 00:00:00'), ('is_later', '=', True)], string='العملاء')
-    cust_tree_ids = fields.One2many(domain=['|', ('create_date', '<', '2025-08-08 00:00:00'), ('status_contact', 'in', ['accepted', 'rejected'])], string='العملاء')
+    #This domain shows all customers whose record was created before (the date of lunching the deferred sales))OR whose status_contact is either “accepted” or “rejected.”
+    #    cust_tree_ids = fields.One2many(domain=['|', ('create_date', '<', '2025-08-08 00:00:00'), ('status_contact', 'in', ['accepted', 'rejected'])], string='العملاء')
+    cust_tree_ids = fields.One2many( string='العملاء')
     is_later = fields.Boolean(
         related='cust_tree_ids.is_later',
         store=True,
